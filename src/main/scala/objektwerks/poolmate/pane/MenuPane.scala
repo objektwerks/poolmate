85aea3867c8af294d495f7c2aa93e3ad1e0033a6
objektwerks <objektwerks@email.com>
2017-04-15 08:09:28 -0400

    added menubar

---
+++ src/main/scala/objektwerks/poolmate/pane/MenuPane.scala
@@ -0,0 +1,24 @@
+package objektwerks.poolmate.pane
+
+import com.typesafe.config.Config
+import objektwerks.poolmate.App
+
+import scalafx.application.Platform
+import scalafx.scene.control.Alert.AlertType
+import scalafx.scene.control._
+
+class MenuPane(conf: Config) extends MenuBar {
+  val aboutDialog = new Alert(AlertType.Information) {
+    initOwner(App.stage)
+    title = conf.getString("about")
+    headerText = conf.getString("developer")
+    contentText = s"${conf.getString("app")} ${conf.getString("license")}"
+  }
+  val aboutMenuItem = new MenuItem(conf.getString("about")) { onAction = { _ => aboutDialog.showAndWait() } }
+  val separator = new SeparatorMenuItem()
+  val exitMenuItem = new MenuItem(conf.getString("exit")) { onAction = { _ => Platform.exit() } }
+  val menu = new Menu(conf.getString("menu")) { items = List(aboutMenuItem, separator, exitMenuItem) }
+
+  menus = List(menu)
+  useSystemMenuBar = false
+}
--- src/main/scala/objektwerks/poolmate/view/View.scala
+++ src/main/scala/objektwerks/poolmate/view/View.scala
@@ -2,13 +2,15 @@ package objektwerks.poolmate.view
 
 import com.typesafe.config.Config
 import objektwerks.poolmate.model.Model
+import objektwerks.poolmate.pane.MenuPane
 
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
 import scalafx.scene.layout.VBox
 
 class View(conf: Config, model: Model) {
-  val contentPane = new VBox { prefHeight = 600; prefWidth = 800; spacing = 6; padding = Insets(6); children = List() }
+  val menuPane = new MenuPane(conf)
+  val contentPane = new VBox { prefHeight = 600; prefWidth = 800; spacing = 6; padding = Insets(6); children = List(menuPane) }
   val sceneGraph = new Scene { root = contentPane }
 
   model.listOwners()
