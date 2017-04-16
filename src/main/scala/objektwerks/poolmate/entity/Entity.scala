4e5394126f95e265504e2333dbea139c0e72e61e
objektwerks <objektwerks@email.com>
2017-04-16 07:10:11 -0400

    edited entities

--- src/main/scala/objektwerks/poolmate/entity/Entity.scala
+++ src/main/scala/objektwerks/poolmate/entity/Entity.scala
@@ -6,9 +6,13 @@ case class Owner(id: Int = 0, name: String, email: String, street: String, city:
 
 case class Pool(id: Int = 0, ownerid: Int, gallons: Double = 1000.00)
 
-case class Cleaning(id: Int = 0, poolid: Int, on: LocalDate = LocalDate.now, deck: Boolean = true, brush: Boolean = true, net: Boolean = true, vacuum: Boolean = false, skimmerBasket: Boolean = true, pumpBasket: Boolean = false, pumpFilter: Boolean = false)
+case class Cleaning(id: Int = 0, poolid: Int, on: LocalDate = LocalDate.now, deck: Boolean = true, brush: Boolean = true,
+                    net: Boolean = true, vacuum: Boolean = false, skimmerBasket: Boolean = true, pumpBasket: Boolean = false,
+                    pumpFilter: Boolean = false)
 
-case class Measurement(id: Int = 0, poolid: Int, on: LocalDate = LocalDate.now, temp: Int = 75, totalHardness: Int = 375, totalChlorine: Int = 3, totalBromine: Int = 5, freeChlorine: Int = 3, pH: Double = 7.5, totalAlkalinity: Int = 100, cyanuricAcid: Int = 50)
+case class Measurement(id: Int = 0, poolid: Int, on: LocalDate = LocalDate.now, temp: Int = 75, totalHardness: Int = 375,
+                       totalChlorine: Int = 3, totalBromine: Int = 5, freeChlorine: Int = 3, pH: Double = 7.5, totalAlkalinity: Int = 100,
+                       cyanuricAcid: Int = 50)
 
 case class Chemical(id: Int = 0, name: String, unit: String)
 
