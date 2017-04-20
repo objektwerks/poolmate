b8f1b6ddea57fa9972bfe35c794c57dac2583363
objektwerks <objektwerks@email.com>
2017-04-20 18:15:11 -0400

    added pool model

--- src/main/scala/objektwerks/poolmate/model/Model.scala
+++ src/main/scala/objektwerks/poolmate/model/Model.scala
@@ -1,6 +1,6 @@
 package objektwerks.poolmate.model
 
-import objektwerks.poolmate.entity.Owner
+import objektwerks.poolmate.entity.{Owner, Pool}
 import objektwerks.poolmate.entity.Entity._
 import objektwerks.poolmate.repository.Repository
 
@@ -10,6 +10,29 @@ import scalafx.collections.ObservableBuffer
 class Model(repository: Repository) {
   import repository._
 
+  val poolList = ObservableBuffer[Pool]()
+  val selectedPoolId = ObjectProperty[Int](0)
+
+  def listPools(): Unit = {
+    poolList.clear()
+    ownerList.clear()
+    poolList ++= await(pools.list())
+  }
+
+  def addPool(pool: Pool): Pool = {
+    val newId = await(pools.save(pool))
+    val newPool = pool.copy(id = newId.get)
+    poolList += newPool
+    selectedPoolId.value = newPool.id
+    newPool
+  }
+
+  def updatePool(selectedIndex: Int, pool: Pool): Unit = {
+    await(pools.save(pool))
+    poolList.update(selectedIndex, pool)
+    poolList.sorted
+  }
+
   val ownerList = ObservableBuffer[Owner]()
   val selectedOwnerId = ObjectProperty[Int](0)
 
@@ -18,17 +41,17 @@ class Model(repository: Repository) {
     ownerList ++= await(owners.list())
   }
 
-  def updateOwner(selectedIndex: Int, owner: Owner): Unit = {
-    await(owners.save(owner))
-    ownerList.update(selectedIndex, owner)
-    ownerList.sorted
-  }
-
   def addOwner(owner: Owner): Owner = {
     val newId = await(owners.save(owner))
     val newOwner = owner.copy(id = newId.get)
     ownerList += newOwner
     selectedOwnerId.value = newOwner.id
     newOwner
+  }
+
+  def updateOwner(selectedIndex: Int, owner: Owner): Unit = {
+    await(owners.save(owner))
+    ownerList.update(selectedIndex, owner)
+    ownerList.sorted
   }
 }
