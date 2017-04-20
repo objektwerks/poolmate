8f8c185016de6f2dd65d5cabff3adcc4c05acd89
objektwerks <objektwerks@email.com>
2017-04-20 18:03:13 -0400

    reformatted view code

--- src/main/scala/objektwerks/poolmate/view/View.scala
+++ src/main/scala/objektwerks/poolmate/view/View.scala
@@ -10,10 +10,12 @@ import scalafx.scene.control.SplitPane
 import scalafx.scene.layout.{Priority, VBox}
 
 class View(conf: Config, model: Model) {
-  val menuPane = new MenuPane(conf)
   val westPane = new VBox { spacing = 6; padding = Insets(6); children = List() }
   val eastPane = new VBox { spacing = 6; padding = Insets(6); children = List() }
+
+  val menuPane = new MenuPane(conf)
   val splitPane = new SplitPane { vgrow = Priority.Always; hgrow = Priority.Always; padding = Insets(6); items.addAll(westPane, eastPane) }
+
   val contentPane = new VBox { prefHeight = 600; prefWidth = 800; spacing = 6; padding = Insets(6); children = List(menuPane, splitPane) }
   val sceneGraph = new Scene { root = contentPane }
 
