9c250468a2f0617b852af5f91249db3a01240765
objektwerks <objektwerks@email.com>
2017-04-20 16:05:11 -0400

    refactored view to compose a classic west-east split pane view

--- src/main/scala/objektwerks/poolmate/view/View.scala
+++ src/main/scala/objektwerks/poolmate/view/View.scala
@@ -6,10 +6,14 @@ import objektwerks.poolmate.pane.MenuPane
 
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
-import scalafx.scene.layout.VBox
+import scalafx.scene.control.SplitPane
+import scalafx.scene.layout.{Priority, VBox}
 
 class View(conf: Config, model: Model) {
   val menuPane = new MenuPane(conf)
+  val westPane = new VBox { spacing = 6; padding = Insets(6); children = List() }
+  val eastPane = new VBox { spacing = 6; padding = Insets(6); children = List() }
+  val splitPane = new SplitPane { vgrow = Priority.Always; hgrow = Priority.Always; padding = Insets(6); items.addAll(westPane, eastPane) }
   val contentPane = new VBox { prefHeight = 600; prefWidth = 800; spacing = 6; padding = Insets(6); children = List(menuPane) }
   val sceneGraph = new Scene { root = contentPane }
 
