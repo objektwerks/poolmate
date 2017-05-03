947cc94c24febb9dda207dad35049ef95776393f
objektwerks <objektwerks@email.com>
2017-05-03 13:52:41 -0400

    edited app height and width; edited split pane to autosize and divide by percentage

--- src/main/scala/objektwerks/poolmate/view/View.scala
+++ src/main/scala/objektwerks/poolmate/view/View.scala
@@ -28,6 +28,8 @@ class View(conf: Config, model: Model) {
 
   val menuPane = new MenuPane(conf)
   val splitPane = new SplitPane { vgrow = Priority.Always; hgrow = Priority.Always; padding = Insets(6); items.addAll(westPane, eastPane) }
+  splitPane.autosize()
+  splitPane.setDividerPositions(0.3, 0.7)
 
   val contentPane = new VBox { prefHeight = 600; prefWidth = 800; spacing = 6; padding = Insets(6); children = List(menuPane, splitPane) }
   val sceneGraph = new Scene { root = contentPane }
