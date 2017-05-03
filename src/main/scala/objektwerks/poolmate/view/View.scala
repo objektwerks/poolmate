bfcc7288bcd8ffddc429983ee6e67346d4857b86
objektwerks <objektwerks@email.com>
2017-05-03 16:44:38 -0400

    added tabs to the view

--- src/main/scala/objektwerks/poolmate/view/View.scala
+++ src/main/scala/objektwerks/poolmate/view/View.scala
@@ -4,32 +4,54 @@ import com.typesafe.config.Config
 import objektwerks.poolmate.model.Model
 import objektwerks.poolmate.pane._
 
-import scalafx.geometry.Insets
+import scalafx.collections.ObservableBuffer
+import scalafx.geometry.{Insets, Orientation}
 import scalafx.scene.Scene
-import scalafx.scene.control.SplitPane
-import scalafx.scene.layout.{Priority, VBox}
+import scalafx.scene.control.{SplitPane, Tab, TabPane}
+import scalafx.scene.layout.{HBox, Priority, VBox}
 
 class View(conf: Config, model: Model) {
   val poolPane = new PoolPane(conf, model)
   val ownerPane = new OwnerPane(conf, model)
+  val northPane = new HBox { spacing = 6; padding = Insets(6); children = List(poolPane, ownerPane) }
+
   val surfacePane = new SurfacePane(conf, model)
   val timerPane = new TimerPane(conf, model)
   val pumpPane = new PumpPane(conf, model)
   val heaterPane = new HeaterPane(conf, model)
-  val westPane = new VBox { spacing = 6; padding = Insets(6); children = List(poolPane, ownerPane, surfacePane, pumpPane, timerPane, heaterPane) }
+  val partsContent = new VBox { spacing = 6; padding = Insets(6); children = List(surfacePane, timerPane, pumpPane, heaterPane) }
+  val partsTab = new Tab { text = conf.getString("parts-tab"); content = partsContent }
+
+  val repairPane = new RepairPane(conf, model)
+  val repairsContent = new VBox { spacing = 6; padding = Insets(6); children = List(repairPane) }
+  val repairsTab = new Tab { text = conf.getString("repairs-tab"); content = repairsContent }
+
+  val supplyPane = new SupplyPane(conf, model)
+  val suppliesContent = new VBox { spacing = 6; padding = Insets(6); children = List(supplyPane) }
+  val suppliesTab = new Tab { text = conf.getString("supplies-tab"); content = suppliesContent }
 
   val lifecyclePane = new LifecyclePane(conf, model)
+  val timersContent = new VBox { spacing = 6; padding = Insets(6); children = List(lifecyclePane) }
+  val timersTab = new Tab { text = conf.getString("timers-tab"); content = timersContent }
+
   val cleaningPane = new CleaningPane(conf, model)
+  val cleaningsContent = new VBox { spacing = 6; padding = Insets(6); children = List(cleaningPane) }
+  val cleaningsTab = new Tab { text = conf.getString("cleanings-tab"); content = cleaningsContent }
+
   val measurementPane = new MeasurementPane(conf, model)
+  val measurementsContent = new VBox { spacing = 6; padding = Insets(6); children = List(measurementPane) }
+  val measurementsTab = new Tab { text = conf.getString("measurements-tab"); content = measurementsContent }
+
   val additivePane = new AdditivePane(conf, model)
-  val supplyPane = new SupplyPane(conf, model)
-  val repairPane = new RepairPane(conf, model)
-  val eastPane = new VBox { spacing = 6; padding = Insets(6); children = List(lifecyclePane, cleaningPane, measurementPane, additivePane, supplyPane, repairPane) }
+  val additivesContent = new VBox { spacing = 6; padding = Insets(6); children = List(additivePane) }
+  val additivesTab = new Tab { text = conf.getString("additives-tab"); content = additivesContent }
+
+  val southPane = new TabPane { padding = Insets(6); tabs = ObservableBuffer(partsTab, repairsTab, suppliesTab, timersTab, cleaningsTab, measurementsTab, additivesTab) }
 
   val menuPane = new MenuPane(conf)
-  val splitPane = new SplitPane { vgrow = Priority.Always; hgrow = Priority.Always; padding = Insets(6); items.addAll(westPane, eastPane) }
-  splitPane.autosize()
+  val splitPane = new SplitPane { orientation = Orientation.Vertical ; vgrow = Priority.Always; hgrow = Priority.Always; padding = Insets(6); items.addAll(northPane, southPane) }
   splitPane.setDividerPositions(0.3, 0.7)
+  splitPane.autosize()
 
   val contentPane = new VBox { prefHeight = 600; prefWidth = 800; spacing = 6; padding = Insets(6); children = List(menuPane, splitPane) }
   val sceneGraph = new Scene { root = contentPane }
