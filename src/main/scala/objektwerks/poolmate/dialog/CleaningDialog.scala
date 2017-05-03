ab9dece006f4bb1e1883e8abf530b7731f1a4c3e
objektwerks <objektwerks@email.com>
2017-05-03 12:47:33 -0400

    added cleaning pane and dialog

---
+++ src/main/scala/objektwerks/poolmate/dialog/CleaningDialog.scala
@@ -0,0 +1,57 @@
+package objektwerks.poolmate.dialog
+
+import com.typesafe.config.Config
+import objektwerks.poolmate.App
+import objektwerks.poolmate.entity.Cleaning
+import objektwerks.poolmate.pane.ControlGridPane
+
+import scalafx.Includes._
+import scalafx.scene.control.ButtonBar.ButtonData
+import scalafx.scene.control._
+import scalafx.scene.layout.Region
+
+class CleaningDialog(conf: Config, cleaning: Cleaning) extends Dialog[Cleaning]() {
+  val saveButtonType = new ButtonType(conf.getString("save"), ButtonData.OKDone)
+  val onDatePicker = new DatePicker { value = cleaning.on }
+  val deckCheckBox = new CheckBox { selected = cleaning.deck }
+  val brushCheckBox = new CheckBox { selected = cleaning.brush }
+  val netCheckBox = new CheckBox { selected = cleaning.net }
+  val vacuumCheckBox = new CheckBox { selected = cleaning.vacuum }
+  val skimmerBasketCheckBox = new CheckBox { selected = cleaning.skimmerBasket }
+  val pumpBasketCheckBox = new CheckBox { selected = cleaning.pumpBasket }
+  val pumpFilterCheckBox = new CheckBox { selected = cleaning.pumpFilter }
+
+  val controls = List[(String, Region)](
+    conf.getString("cleaning-on") -> onDatePicker,
+    conf.getString("cleaning-deck") -> deckCheckBox,
+    conf.getString("cleaning-brush") -> brushCheckBox,
+    conf.getString("cleaning-net") -> netCheckBox,
+    conf.getString("cleaning-vacuum") -> vacuumCheckBox,
+    conf.getString("cleaning-skimmer-basket") -> skimmerBasketCheckBox,
+    conf.getString("cleaning-pump-basket") -> pumpBasketCheckBox,
+    conf.getString("cleaning-pump-filter") -> pumpFilterCheckBox
+  )
+  val controlGridPane = new ControlGridPane(controls)
+  val dialog = dialogPane()
+  dialog.buttonTypes = List(saveButtonType, ButtonType.Cancel)
+  dialog.content = controlGridPane
+
+  initOwner(App.stage)
+  title = conf.getString("title")
+  headerText = conf.getString("save-cleaning")
+
+  val saveButton = dialog.lookupButton(saveButtonType)
+
+  resultConverter = dialogButton => {
+    if (dialogButton == saveButtonType)
+      cleaning.copy(on = onDatePicker.value.value,
+                    deck = deckCheckBox.selected.value,
+                    brush = deckCheckBox.selected.value,
+                    net = netCheckBox.selected.value,
+                    vacuum = vacuumCheckBox.selected.value,
+                    skimmerBasket = skimmerBasketCheckBox.selected.value,
+                    pumpBasket = pumpBasketCheckBox.selected.value,
+                    pumpFilter = pumpFilterCheckBox.selected.value)
+    else null
+  }
+}
---
+++ src/main/scala/objektwerks/poolmate/pane/CleaningPane.scala
@@ -0,0 +1,78 @@
+package objektwerks.poolmate.pane
+
+import com.typesafe.config.Config
+import objektwerks.poolmate.dialog.CleaningDialog
+import objektwerks.poolmate.entity.Cleaning
+import objektwerks.poolmate.image.Images
+import objektwerks.poolmate.model.Model
+
+import scalafx.Includes._
+import scalafx.scene.control.TableColumn._
+import scalafx.scene.control._
+import scalafx.scene.layout.{HBox, VBox}
+
+class CleaningPane(conf: Config, model: Model) extends VBox {
+  val cleaningLabel = new Label { text = conf.getString("cleanings") }
+  val cleaningTableView = new TableView[Cleaning]() {
+    columns ++= List(
+      new TableColumn[Cleaning, String] { text = conf.getString("cleaning-table-column-on"); cellValueFactory = { _.value.onProperty } },
+      new TableColumn[Cleaning, String] { text = conf.getString("cleaning-table-column-deck"); cellValueFactory = { _.value.deckProperty } },
+      new TableColumn[Cleaning, String] { text = conf.getString("cleaning-table-column-brush"); cellValueFactory = { _.value.brushProperty } },
+      new TableColumn[Cleaning, String] { text = conf.getString("cleaning-table-column-net"); cellValueFactory = { _.value.netProperty } },
+      new TableColumn[Cleaning, String] { text = conf.getString("cleaning-table-column-vacuum"); cellValueFactory = { _.value.vacuumProperty } },
+      new TableColumn[Cleaning, String] { text = conf.getString("cleaning-table-column-skimmer-basket"); cellValueFactory = { _.value.skimmerBasketProperty } },
+      new TableColumn[Cleaning, String] { text = conf.getString("cleaning-table-column-pump-basket"); cellValueFactory = { _.value.pumpBasketProperty } },
+      new TableColumn[Cleaning, String] { text = conf.getString("cleaning-table-column-pump-filter"); cellValueFactory = { _.value.pumpFilterProperty } }
+    )
+    prefHeight = 100
+    items = model.cleaningList
+  }
+  cleaningTableView.selectionModel().selectionModeProperty.value = SelectionMode.Single
+  val cleaningAddButton = new Button { graphic = Images.addImageView() }
+  val cleaningEditButton = new Button { graphic = Images.editImageView(); disable = true }
+  val cleaningToolBar = new HBox { spacing = 6; children = List(cleaningAddButton, cleaningEditButton) }
+
+  spacing = 6
+  children = List(cleaningLabel, cleaningTableView, cleaningToolBar)
+
+  model.selectedPoolId.onChange { (_, _, selectedPoolId) =>
+    model.listCleanings(selectedPoolId.intValue)
+    cleaningAddButton.disable = false
+  }
+
+  cleaningTableView.selectionModel().selectedItemProperty().addListener { (_, _, selectedCleaning) =>
+    // model.update executes a remove and add on items. the remove passes a null selectedCleaning!
+    if (selectedCleaning != null) {
+      model.selectedCleaningId.value = selectedCleaning.id
+      cleaningEditButton.disable = false
+    }
+  }
+
+  cleaningTableView.onMouseClicked = { event =>
+    if(event.getClickCount == 2 && cleaningTableView.selectionModel().getSelectedItem != null ) update()
+  }
+
+  cleaningAddButton.onAction = { _ => add() }
+
+  cleaningEditButton.onAction = { _ => update() }
+
+  def add(): Unit = {
+    new CleaningDialog(conf, Cleaning(poolId = model.selectedPoolId.toInt)).showAndWait() match {
+      case Some(Cleaning(id, poolId, on, deck, brush, net, vacuum, skimmerBasket, pumpBasket, pumpFilter)) =>
+        val newCleaning = model.addCleaning(Cleaning(id, poolId, on, deck, brush, net, vacuum, skimmerBasket, pumpBasket, pumpFilter))
+        cleaningTableView.selectionModel().select(newCleaning)
+      case _ =>
+    }
+  }
+
+  def update(): Unit = {
+    val selectedIndex = cleaningTableView.selectionModel().getSelectedIndex
+    val cleaning = cleaningTableView.selectionModel().getSelectedItem.cleaning
+    new CleaningDialog(conf, cleaning).showAndWait() match {
+      case Some(Cleaning(id, poolId, on, deck, brush, net, vacuum, skimmerBasket, pumpBasket, pumpFilter)) =>
+        model.updateCleaning(selectedIndex, Cleaning(id, poolId, on, deck, brush, net, vacuum, skimmerBasket, pumpBasket, pumpFilter))
+        cleaningTableView.selectionModel().select(selectedIndex)
+      case _ =>
+    }
+  }
+}
