dab95f673d66eca85dc0aa62c9e23a3e45c18fc3
objektwerks <objektwerks@runbox.com>
2020-10-26 10:12:05 -0400

    refactored entity

--- src/main/scala/objektwerks/poolmate/entity/Entity.scala
+++ src/main/scala/objektwerks/poolmate/entity/Entity.scala
@@ -5,99 +5,151 @@ import java.time.{LocalDate, LocalDateTime, LocalTime, ZoneOffset}
 
 import scalafx.beans.property.StringProperty
 
-case class Company(id: Int = 0, name: String = "name", since: LocalDate = LocalDate.now, website: String = "website", email: String = "email")
+sealed trait Entity extends Product with Serializable
 
-case class Worker(id: Int = 0, companyId: Int, hired: LocalDate = LocalDate.now, terminated: Option[LocalDate], first: String = "first", last: String = "last", email: String)
+final case class Company(id: Int = 0,
+                         name: String = "name", 
+                         since: LocalDate = LocalDate.now, 
+                         website: String = "website", 
+                         email: String = "email") extends Entity
 
-case class WorkOrder(id: Int = 0, poolId: Int, description: String = "description", created: LocalDate = LocalDate.now, completed: Option[LocalDateTime])
+final case class Worker(id: Int = 0,
+                        companyId: Int, 
+                        hired: LocalDate = LocalDate.now, 
+                        terminated: Option[LocalDate], 
+                        first: String = "first", 
+                        last: String = "last", 
+                        email: String) extends Entity
 
-case class WorkOrderWorker(id: Int = 0, workOrderId: Int, workerId: Int)
+final case class WorkOrder(id: Int = 0, 
+                           poolId: Int, 
+                           description: String = "description", 
+                           created: LocalDate = LocalDate.now, 
+                           completed: Option[LocalDateTime]) extends Entity
 
-case class RouteOrder(id: Int = 0, routeId: Int, created: LocalDate = LocalDate.now, completed: LocalDate = LocalDate.now, recurring: Boolean = true)
+final case class WorkOrderWorker(id: Int = 0, 
+                                 workOrderId: Int, 
+                                 workerId: Int) extends Entity
 
-case class RouteOrderWorker(id: Int = 0, routeOrderId: Int, workerId: Int)
+final case class RouteOrder(id: Int = 0, 
+                            routeId: Int, 
+                            created: LocalDate = LocalDate.now, 
+                            completed: LocalDate = LocalDate.now, 
+                            recurring: Boolean = true) extends Entity
 
-case class Location(id: Int = 0, routeOrderId: Int, poolId: Int, ordinality: Int, completed: Option[LocalDateTime])
+final case class RouteOrderWorker(id: Int = 0, 
+                                  routeOrderId: Int, 
+                                  workerId: Int) extends Entity
 
-case class Route(id: Int = 0, name: String)
+final case class Location(id: Int = 0, 
+                          routeOrderId: Int, 
+                          poolId: Int, 
+                          ordinality: Int, 
+                          completed: Option[LocalDateTime]) extends Entity
 
-case class Stop(routeId: Int, poolId: Int, ordinality: Int)
+final case class Route(id: Int = 0, 
+                       name: String) extends Entity
 
-case class Pool(id: Int = 0, built: LocalDate = LocalDate.now, gallons: Int = 10000, street: String = "street", city: String = "city", state: String = "state", zip: Int = 12345) {
+final case class Stop(routeId: Int, 
+                      poolId: Int, 
+                      ordinality: Int) extends Entity
+
+final case class Pool(id: Int = 0, 
+                      built: LocalDate = LocalDate.now, 
+                      gallons: Int = 10000, 
+                      street: String = "street", 
+                      city: String = "city", 
+                      state: String = "state", 
+                      zip: Int = 12345) extends Entity {
   val dateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-
   val builtProperty = new StringProperty(this, "built", built.format(dateFormatter))
   val gallonsProperty = new StringProperty(this, "gallons", gallons.toString)
   val streetProperty = new StringProperty(this, "street", street)
   val cityProperty = new StringProperty(this, "city", city)
   val stateProperty = new StringProperty(this, "state", state)
   val zipProperty = new StringProperty(this, "zip", zip.toString)
-
   val pool = this
 }
 
-case class Owner(id: Int = 0, poolId: Int, since: LocalDate = LocalDate.now, first: String = "first", last: String = "last", email: String = "your@email.org") {
+final case class Owner(id: Int = 0, 
+                       poolId: Int, 
+                       since: LocalDate = LocalDate.now, 
+                       first: String = "first", 
+                       last: String = "last", 
+                       email: String = "your@email.org") extends Entity {
   val dateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-
   val sinceProperty = new StringProperty(this, "since", since.format(dateFormatter))
   val firstProperty = new StringProperty(this, "first", first)
   val lastroperty = new StringProperty(this, "last", last)
-
   val owner = this
 }
 
-case class Surface(id: Int = 0, poolId: Int, installed: LocalDate = LocalDate.now, kind: String = "kind") {
+final case class Surface(id: Int = 0, 
+                         poolId: Int, 
+                         installed: LocalDate = LocalDate.now, 
+                         kind: String = "kind") extends Entity {
   val dateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-
   val installedProperty = new StringProperty(this, "installed", installed.format(dateFormatter))
   val kindProperty = new StringProperty(this, "kind", kind)
-
   val surface = this
 }
 
-case class Pump(id: Int = 0, poolId: Int, installed: LocalDate = LocalDate.now, model: String = "model") {
+final case class Pump(id: Int = 0, 
+                      poolId: Int, 
+                      installed: LocalDate = LocalDate.now, 
+                      model: String = "model") extends Entity {
   val dateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-
   val installedProperty = new StringProperty(this, "installed", installed.format(dateFormatter))
   val modelProperty = new StringProperty(this, "model", model)
-
   val pump = this
 }
 
-case class Timer(id: Int = 0, poolId: Int, installed: LocalDate = LocalDate.now, model: String = "model") {
+final case class Timer(id: Int = 0, 
+                       poolId: Int, 
+                       installed: LocalDate = LocalDate.now, 
+                       model: String = "model") extends Entity {
   val dateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-
   val installedProperty = new StringProperty(this, "installed", installed.format(dateFormatter))
   val modelProperty = new StringProperty(this, "model", model)
-
   val timer = this
 }
 
-case class Heater(id: Int = 0, poolId: Int, installed: LocalDate = LocalDate.now, model: String = "model") {
+final case class Heater(id: Int = 0, 
+                        poolId: Int, 
+                        installed: LocalDate = LocalDate.now, 
+                        model: String = "model") extends Entity {
   val dateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-
   val installedProperty = new StringProperty(this, "installed", installed.format(dateFormatter))
   val modelProperty = new StringProperty(this, "model", model)
-
   val heater = this
 }
 
-case class Lifecycle(id: Int = 0, poolId: Int, created: LocalDate = LocalDate.now, active: Boolean = true, pumpOn: LocalTime = LocalTime.of(9, 0), pumpOff: LocalTime = LocalTime.of(17, 0)) {
+final case class Lifecycle(id: Int = 0, 
+                           poolId: Int, 
+                           created: LocalDate = LocalDate.now, 
+                           active: Boolean = true, 
+                           pumpOn: LocalTime = LocalTime.of(9, 0), 
+                           pumpOff: LocalTime = LocalTime.of(17, 0)) extends Entity {
   val dateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
   val timeFormatter = DateTimeFormatter.ofPattern("hh:mm")
-
   val createdProperty = new StringProperty(this, "created", created.format(dateFormatter))
   val activeProperty = new StringProperty(this, "active", active.toString)
   val pumpOnProperty = new StringProperty(this, "pumpOn", pumpOn.format(timeFormatter))
   val pumpOffProperty = new StringProperty(this, "pumpOff", pumpOff.format(timeFormatter))
-
   val lifecycle = this
 }
 
-case class Cleaning(id: Int = 0, poolId: Int, on: LocalDate = LocalDate.now, deck: Boolean = true, brush: Boolean = true, net: Boolean = true,
-                    vacuum: Boolean = false, skimmerBasket: Boolean = true, pumpBasket: Boolean = false, pumpFilter: Boolean = false) {
+final case class Cleaning(id: Int = 0, 
+                          poolId: Int, 
+                          on: LocalDate = LocalDate.now, 
+                          deck: Boolean = true, 
+                          brush: Boolean = true, 
+                          net: Boolean = true,
+                          vacuum: Boolean = false, 
+                          skimmerBasket: Boolean = true, 
+                          pumpBasket: Boolean = false, 
+                          pumpFilter: Boolean = false) extends Entity {
   val dateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-
   val onProperty = new StringProperty(this, "on", on.format(dateFormatter))
   val deckProperty = new StringProperty(this, "deck", deck.toString)
   val brushProperty = new StringProperty(this, "brush", brush.toString)
@@ -106,14 +158,21 @@ case class Cleaning(id: Int = 0, poolId: Int, on: LocalDate = LocalDate.now, dec
   val skimmerBasketProperty = new StringProperty(this, "skimmerBasket", skimmerBasket.toString)
   val pumpBasketProperty = new StringProperty(this, "pumpBasket", pumpBasket.toString)
   val pumpFilterProperty = new StringProperty(this, "pumpFilter", pumpFilter.toString)
-
   val cleaning = this
 }
 
-case class Measurement(id: Int = 0, poolId: Int, on: LocalDate = LocalDate.now, temp: Double = 75.0, hardness: Double = 375.0, totalChlorine: Double = 3,
-                       bromine: Double = 5.0, freeChlorine: Double = 3.0, pH: Double = 7.5, alkalinity: Double = 100.0, cyanuricAcid: Double = 50.0) {
+final case class Measurement(id: Int = 0, 
+                             poolId: Int, 
+                             on: LocalDate = LocalDate.now, 
+                             temp: Double = 75.0, 
+                             hardness: Double = 375.0, 
+                             totalChlorine: Double = 3,
+                             bromine: Double = 5.0, 
+                             freeChlorine: Double = 3.0, 
+                             pH: Double = 7.5, 
+                             alkalinity: Double = 100.0, 
+                             cyanuricAcid: Double = 50.0) extends Entity {
   val dateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-
   val onProperty = new StringProperty(this, "on", on.format(dateFormatter))
   val tempProperty = new StringProperty(this, "temp", temp.toString)
   val hardnessProperty = new StringProperty(this, "hardness", hardness.toString)
@@ -123,85 +182,72 @@ case class Measurement(id: Int = 0, poolId: Int, on: LocalDate = LocalDate.now, 
   val pHProperty = new StringProperty(this, "pH", pH.toString)
   val alkalinityProperty = new StringProperty(this, "alkalinity", alkalinity.toString)
   val cyanuricAcidProperty = new StringProperty(this, "cyanuricAcid", cyanuricAcid.toString)
-
   val measurement = this
 }
 
-case class Additive(id: Int = 0, poolId: Int, on: LocalDate = LocalDate.now, chemical: String = "cl", unit: String = "gl", amount: Double = 1.0) {
+final case class Additive(id: Int = 0, 
+                          poolId: Int, 
+                          on: LocalDate = LocalDate.now, 
+                          chemical: String = "cl", 
+                          unit: String = "gl", 
+                          amount: Double = 1.0) extends Entity {
   val dateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-
   val onProperty = new StringProperty(this, "on", on.format(dateFormatter))
   val chemicalProperty = new StringProperty(this, "chemical", chemical)
   val unitProperty = new StringProperty(this, "unit", unit)
   val amountProperty = new StringProperty(this, "amount", amount.toString)
-
   val additive = this
 }
 
-case class Supply(id: Int = 0, poolId: Int, purchased: LocalDate = LocalDate.now, item: String = "cl", unit: String = "gl", amount: Double = 1.0, cost: Double = 0.0) {
+final case class Supply(id: Int = 0, 
+                        poolId: Int, 
+                        purchased: LocalDate = LocalDate.now, 
+                        item: String = "cl", 
+                        unit: String = "gl", 
+                        amount: Double = 1.0, 
+                        cost: Double = 0.0) extends Entity {
   val dateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-
   val purchasedProperty = new StringProperty(this, "purchased", purchased.format(dateFormatter))
   val itemProperty = new StringProperty(this, "item", item)
   val unitProperty = new StringProperty(this, "unit", unit)
   val amountProperty = new StringProperty(this, "amount", amount.toString)
   val costProperty = new StringProperty(this, "cost", cost.toString)
-
   val supply = this
 }
 
-case class Repair(id: Int = 0, poolId: Int, on: LocalDate = LocalDate.now, item: String = "repair", cost: Double = 0.0) {
+final case class Repair(id: Int = 0, 
+                        poolId: Int, 
+                        on: LocalDate = LocalDate.now, 
+                        item: String = "repair", 
+                        cost: Double = 0.0) extends Entity {
   val dateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-
   val onProperty = new StringProperty(this, "on", on.format(dateFormatter))
   val itemProperty = new StringProperty(this, "item", item)
   val costProperty = new StringProperty(this, "cost", cost.toString)
-
   val repair = this
 }
 
 object Entity {
   implicit def localTimeOrdering: Ordering[LocalTime] = Ordering.by(_.toSecondOfDay)
-
   implicit def localDateOrdering: Ordering[LocalDate] = Ordering.by(_.toEpochDay)
-
   implicit def localDateTimeOrdering: Ordering[LocalDateTime] = Ordering.by(_.toEpochSecond(ZoneOffset.UTC))
-
   implicit def companyOrdering: Ordering[Company] = Ordering.by(_.name)
-
   implicit def workerOrdering: Ordering[Worker] = Ordering.by(_.last)
-
   implicit def workOrderOrdering: Ordering[WorkOrder] = Ordering.by(_.created)
-
   implicit def routeOrderOrdering: Ordering[RouteOrder] = Ordering.by(ro => (ro.created, ro.completed, ro.recurring))
-
   implicit def locationOrdering: Ordering[Location] = Ordering.by(_.ordinality)
-
   implicit def routeOrdering: Ordering[Route] = Ordering.by(_.name)
-
   implicit def stopOrdering: Ordering[Stop] = Ordering.by(_.ordinality)
-
   implicit def poolOrdering: Ordering[Pool] = Ordering.by(p => (p.zip, p.city))
-
   implicit def ownerOrdering: Ordering[Owner] = Ordering.by(o => (o.since, o.last))
-
   implicit def surfaceOrdering: Ordering[Surface] = Ordering.by(_.installed)
-
   implicit def pumpOrdering: Ordering[Pump] = Ordering.by(_.installed)
-
   implicit def timerOrdering: Ordering[Timer] = Ordering.by(_.installed)
-
   implicit def heaterOrdering: Ordering[Heater] = Ordering.by(_.installed)
-
   implicit def lifecycleOrdering: Ordering[Lifecycle] = Ordering.by(l => (l.active, l.created))
-
   implicit def cleaningOrdering: Ordering[Cleaning] = Ordering.by(_.on)
-
   implicit def measurementOrdering: Ordering[Measurement] = Ordering.by(_.on)
-
   implicit def additiveOrdering: Ordering[Additive] = Ordering.by(_.on)
-
   implicit def supplyOrdering: Ordering[Supply] = Ordering.by(_.purchased)
-
   implicit def repairOrdering: Ordering[Repair] = Ordering.by(_.on)
 }
