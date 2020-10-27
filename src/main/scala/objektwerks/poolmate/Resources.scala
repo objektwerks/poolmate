b55dc57cb27240e68c3536e5279a001466853be0
objektwerks <objektwerks@runbox.com>
2020-10-26 20:28:12 -0400

    refactored resources/images

--- src/main/scala/objektwerks/poolmate/Resources.scala
+++ src/main/scala/objektwerks/poolmate/Resources.scala
@@ -9,12 +9,12 @@ import scala.jdk.CollectionConverters._
 
 object Resources {
   val conf = ConfigFactory.load("app.conf")
-  val units = ObservableBuffer[String](conf.getStringList("units").asScala.toSeq)
-  val appImage = new Image(Resources.getClass.getResourceAsStream("/images/pool.png"))
-  val addImageView = loadImageView("/images/add.png")
-  val editImageView = loadImageView("/images/edit.png")
-  val barChartImageView = loadImageView("/images/bar.chart.png")
-  val lineChartImageView = loadImageView("/images/line.chart.png")
+  def units = ObservableBuffer[String](conf.getStringList("units").asScala.toSeq)
+  def appImage = new Image(Resources.getClass.getResourceAsStream("/images/pool.png"))
+  def addImageView = loadImageView("/images/add.png")
+  def editImageView = loadImageView("/images/edit.png")
+  def barChartImageView = loadImageView("/images/bar.chart.png")
+  def lineChartImageView = loadImageView("/images/line.chart.png")
 
   def loadImageView(path: String): ImageView = new ImageView {
     image = new Image(Resources.getClass.getResourceAsStream(path))
