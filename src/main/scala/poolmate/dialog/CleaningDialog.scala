1f6c1b29fd6fdc8f2ce917498272e5dce1429a09
objektwerks <objektwerks@runbox.com>
2022-04-12 08:23:16 -0400

    refactored cleaning dialog package and imports

--- src/main/scala/poolmate/dialog/CleaningDialog.scala
+++ src/main/scala/poolmate/dialog/CleaningDialog.scala
@@ -1,10 +1,10 @@
-package objektwerks.poolmate.dialog
+package poolmate.dialog
 
 import com.typesafe.config.Config
 
-import objektwerks.poolmate.App
-import objektwerks.poolmate.entity.Cleaning
-import objektwerks.poolmate.pane.ControlGridPane
+import poolmate.App
+import poolmate.Cleaning
+import poolmate.pane.ControlGridPane
 
 import scalafx.Includes._
 import scalafx.scene.control.ButtonBar.ButtonData
