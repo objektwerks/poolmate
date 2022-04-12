18348955d36863c2f5503b80a31f7c1959279f89
objektwerks <objektwerks@runbox.com>
2022-04-12 10:49:47 -0400

    refactored imports

--- src/main/scala/poolmate/pane/SurfacePane.scala
+++ src/main/scala/poolmate/pane/SurfacePane.scala
@@ -2,10 +2,9 @@ package poolmate.pane
 
 import com.typesafe.config.Config
 
+import poolmate.{Model, Surface}
 import poolmate.Resources._
 import poolmate.dialog.SurfaceDialog
-import poolmate.Surface
-import poolmate.Model
 
 import scalafx.Includes._
 import scalafx.geometry.Insets
