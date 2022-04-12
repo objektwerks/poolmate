7c1bf4ad087eb393e76cc0da38e80124f21a2020
objektwerks <objektwerks@runbox.com>
2022-04-12 08:13:21 -0400

    refactored additive pane imports

--- src/main/scala/poolmate/pane/CleaningPane.scala
+++ src/main/scala/poolmate/pane/CleaningPane.scala
@@ -1,11 +1,11 @@
-package objektwerks.poolmate.pane
+package poolmate.pane
 
 import com.typesafe.config.Config
 
-import objektwerks.poolmate.Resources._
-import objektwerks.poolmate.dialog.CleaningDialog
-import objektwerks.poolmate.entity.Cleaning
-import objektwerks.poolmate.model.Model
+import poolmate.Resources._
+import poolmate.dialog.CleaningDialog
+import poolmate.Cleaning
+import poolmate.Model
 
 import scalafx.Includes._
 import scalafx.geometry.Insets
