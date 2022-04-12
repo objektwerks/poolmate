89812e30beac2e4fb5d9a8234f2af27ab9b2d05d
objektwerks <objektwerks@runbox.com>
2022-04-12 08:20:12 -0400

    refactored timer pane imports

--- src/main/scala/poolmate/pane/TimerPane.scala
+++ src/main/scala/poolmate/pane/TimerPane.scala
@@ -1,11 +1,11 @@
-package objektwerks.poolmate.pane
+package poolmate.pane
 
 import com.typesafe.config.Config
 
-import objektwerks.poolmate.Resources._
-import objektwerks.poolmate.dialog.TimerDialog
-import objektwerks.poolmate.entity.Timer
-import objektwerks.poolmate.model.Model
+import poolmate.Resources._
+import poolmate.dialog.TimerDialog
+import poolmate.Timer
+import poolmate.Model
 
 import scalafx.Includes._
 import scalafx.geometry.Insets
