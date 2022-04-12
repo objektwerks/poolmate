920d26065dbd64262c2d96dfcded13549cde3f37
objektwerks <objektwerks@runbox.com>
2022-04-12 08:15:51 -0400

    refactored measurement pane imports

--- src/main/scala/poolmate/pane/MeasurementPane.scala
+++ src/main/scala/poolmate/pane/MeasurementPane.scala
@@ -1,11 +1,11 @@
-package objektwerks.poolmate.pane
+package poolmate.pane
 
 import com.typesafe.config.Config
 
-import objektwerks.poolmate.Resources._
-import objektwerks.poolmate.dialog.{MeasurementChartDialog, MeasurementDialog}
-import objektwerks.poolmate.entity.Measurement
-import objektwerks.poolmate.model.Model
+import poolmate.Resources._
+import poolmate.dialog.{MeasurementChartDialog, MeasurementDialog}
+import poolmate.Measurement
+import poolmate.Model
 
 import scalafx.Includes._
 import scalafx.geometry.Insets
