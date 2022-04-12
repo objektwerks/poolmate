3e4e05bd2cd7c930862531e40918d5e30e239d63
objektwerks <objektwerks@runbox.com>
2022-04-12 08:40:18 -0400

    refactored import

--- src/main/scala/poolmate/dialog/PoolDialog.scala
+++ src/main/scala/poolmate/dialog/PoolDialog.scala
@@ -2,8 +2,7 @@ package poolmate.dialog
 
 import com.typesafe.config.Config
 
-import poolmate.App
-import poolmate.Pool
+import poolmate.{App, Pool}
 import poolmate.pane.ControlGridPane
 
 import scalafx.Includes._
