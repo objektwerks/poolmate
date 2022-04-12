b1cfa097d020f119d4eb7d923301c078f71c5516
objektwerks <objektwerks@runbox.com>
2022-04-12 08:24:17 -0400

    refactored lifecycle dialog package and imports

--- src/main/scala/poolmate/dialog/LifecycleDialog.scala
+++ src/main/scala/poolmate/dialog/LifecycleDialog.scala
@@ -1,12 +1,12 @@
-package objektwerks.poolmate.dialog
+package poolmate.dialog
 
 import com.typesafe.config.Config
 
 import jfxtras.scene.control.LocalTimePicker
 
-import objektwerks.poolmate.App
-import objektwerks.poolmate.entity.Lifecycle
-import objektwerks.poolmate.pane.ControlGridPane
+import poolmate.App
+import poolmate.Lifecycle
+import poolmate.pane.ControlGridPane
 
 import scalafx.Includes._
 import scalafx.scene.control.ButtonBar.ButtonData
