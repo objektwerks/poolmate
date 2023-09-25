d8b02d562f55eaf631fa89ea028576ac5fa1a493
Objektwerks <objektwerks@runbox.com>
2023-09-25 10:40:41 -0400

    Update RepositoryTest.scala

--- src/test/scala/poolmate/RepositoryTest.scala
+++ src/test/scala/poolmate/RepositoryTest.scala
@@ -4,11 +4,9 @@ import com.typesafe.config.ConfigFactory
 
 import org.scalatest.funsuite.AnyFunSuite
 
-class RepositoryTest extends AnyFunSuite {
-  test("repository") {
+final class RepositoryTest extends AnyFunSuite:
+  test("repository"):
     val config = ConfigFactory.load("test.conf")
     val repository = Repository(config)
     repository.schema.createStatements foreach println
     repository.close()
-  }
-}
