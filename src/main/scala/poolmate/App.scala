70b53f4f3269321807acc7caa61dce6e33489825
objektwerks <objektwerks@runbox.com>
2024-05-06 07:30:21 -0400

    edited app

--- src/main/scala/poolmate/App.scala
+++ src/main/scala/poolmate/App.scala
@@ -7,23 +7,19 @@ import scalafx.application.JFXApp3
 import slick.basic.DatabaseConfig
 import slick.jdbc.{H2Profile, JdbcProfile}
 
-object App extends JFXApp3 {
+object App extends JFXApp3:
   val config = DatabaseConfig.forConfig[JdbcProfile]("test", ConfigFactory.load("repository.conf"))
-  val repository = new Repository(config, H2Profile)
+  val repository = Repository(config, H2Profile)
   val model = Model(repository)
 
   val resources = ConfigFactory.load("resources.conf")
   val view = View(resources, model)
 
-  override def start(): Unit = {
-    stage = new JFXApp3.PrimaryStage {
+  override def start(): Unit =
+    stage = new JFXApp3.PrimaryStage:
       scene = view.sceneGraph
       title = resources.getString("title")
       icons.add(Resources.appImage)
-    }
-  }
 
-  sys.addShutdownHook {
+  sys.addShutdownHook:
     repository.close()
-  }
-}
