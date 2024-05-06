ead0cbf4cb96d0ac1ab2f1272a6c2e4da85f7727
objektwerks <objektwerks@runbox.com>
2024-05-06 06:57:33 -0400

    refactored additive

--- src/main/scala/poolmate/Entity.scala
+++ src/main/scala/poolmate/Entity.scala
@@ -147,12 +147,11 @@ final case class Measurement(id: Int = 0,
 
 final case class Additive(id: Int = 0, 
                           poolId: Int, 
-                          on: LocalDate = LocalDate.now, 
+                          on: String = LocalDate.now.toString, 
                           chemical: String = "cl", 
                           unit: String = "gl", 
                           amount: Double = 1.0) extends Entity {
-  val dateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-  val onProperty = new StringProperty(this, "on", on.format(dateFormatter))
+  val onProperty = new StringProperty(this, "on", on)
   val chemicalProperty = new StringProperty(this, "chemical", chemical)
   val unitProperty = new StringProperty(this, "unit", unit)
   val amountProperty = new StringProperty(this, "amount", amount.toString)
