f615d1b9bc6198d8ab9f8ec15bb8f986d8478297
objektwerks <objektwerks@runbox.com>
2024-05-06 07:35:36 -0400

    refactored dialogs

--- src/main/scala/poolmate/dialog/Dialogs.scala
+++ src/main/scala/poolmate/dialog/Dialogs.scala
@@ -2,8 +2,7 @@ package poolmate.dialog
 
 import java.time.LocalDate
 
-object Dialogs {
-  implicit def localDateOrdering: Ordering[LocalDate] = Ordering.by(_.toEpochDay)
+object Dialogs:
+  given Ordering[LocalDate] = Ordering.by(_.toEpochDay)
 
   def isNotNumeric(text: String): Boolean = !text.matches("\\d{0,7}([\\.]\\d{0,4})?")
-}
