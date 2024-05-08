9c241fc402d657083469c0e75d834e9cbe3232b3
objektwerks <objektwerks@runbox.com>
2024-05-08 08:05:02 -0400

    reformatted

--- src/main/scala/poolmate/dialog/SurfaceDialog.scala
+++ src/main/scala/poolmate/dialog/SurfaceDialog.scala
@@ -2,26 +2,26 @@ package poolmate.dialog
 
 import com.typesafe.config.Config
 
-import poolmate.{App, Entity, Surface}
-import poolmate.pane.ControlGridPane
-
-import scalafx.Includes._
+import scalafx.Includes.*
 import scalafx.scene.control.ButtonBar.ButtonData
 import scalafx.scene.control.{ButtonType, DatePicker, Dialog, TextField}
 import scalafx.scene.layout.Region
 
-class SurfaceDialog(conf: Config, surface: Surface) extends Dialog[Surface] {
-  val installedDatePicker = new DatePicker {
+import poolmate.{App, Entity, Surface}
+import poolmate.pane.ControlGridPane
+
+class SurfaceDialog(conf: Config, surface: Surface) extends Dialog[Surface]:
+  val installedDatePicker = new DatePicker:
     value = Entity.toLocalDate(surface.installed)
-  }
-  val kindTextField = new TextField {
+
+  val kindTextField = new TextField:
     text = surface.kind
-  }
+
   val controls = List[(String, Region)](
     conf.getString("surface-installed") -> installedDatePicker,
     conf.getString("surface-kind") -> kindTextField
   )
-  val controlGridPane = new ControlGridPane(controls)
+  val controlGridPane = ControlGridPane(controls)
 
   val dialog = dialogPane()
   val saveButtonType = new ButtonType(conf.getString("save"), ButtonData.OKDone)
@@ -31,16 +31,14 @@ class SurfaceDialog(conf: Config, surface: Surface) extends Dialog[Surface] {
   val saveButton = dialog.lookupButton(saveButtonType)
   kindTextField.text.onChange { (_, _, newValue) => saveButton.disable = newValue.trim.isEmpty }
 
-  resultConverter = dialogButton => {
-    if (dialogButton == saveButtonType)
+  resultConverter = dialogButton =>
+    if (dialogButton == saveButtonType) then
       surface.copy(
         installed = installedDatePicker.value.value.toString,
         kind = kindTextField.text.value
       )
     else null
-  }
 
   initOwner(App.stage)
   title = conf.getString("title")
   headerText = conf.getString("save-surface")
-}
