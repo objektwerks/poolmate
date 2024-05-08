74d8eb653fe9ec1e4b7d8b8c3c78e322518a3d42
objektwerks <objektwerks@runbox.com>
2024-05-08 07:59:53 -0400

    reformatted

--- src/main/scala/poolmate/dialog/PumpDialog.scala
+++ src/main/scala/poolmate/dialog/PumpDialog.scala
@@ -2,26 +2,26 @@ package poolmate.dialog
 
 import com.typesafe.config.Config
 
-import poolmate.{App, Entity, Pump}
-import poolmate.pane.ControlGridPane
-
-import scalafx.Includes._
+import scalafx.Includes.*
 import scalafx.scene.control.ButtonBar.ButtonData
 import scalafx.scene.control.{ButtonType, DatePicker, Dialog, TextField}
 import scalafx.scene.layout.Region
 
-class PumpDialog(conf: Config, pump: Pump) extends Dialog[Pump] {
-  val installedDatePicker = new DatePicker {
+import poolmate.{App, Entity, Pump}
+import poolmate.pane.ControlGridPane
+
+class PumpDialog(conf: Config, pump: Pump) extends Dialog[Pump]:
+  val installedDatePicker = new DatePicker:
     value = Entity.toLocalDate(pump.installed)
-  }
-  val modelTextField = new TextField {
+
+  val modelTextField = new TextField:
     text = pump.model
-  }
+
   val controls = List[(String, Region)](
     conf.getString("pump-installed") -> installedDatePicker,
     conf.getString("pump-model") -> modelTextField
   )
-  val controlGridPane = new ControlGridPane(controls)
+  val controlGridPane = ControlGridPane(controls)
 
   val dialog = dialogPane()
   val saveButtonType = new ButtonType(conf.getString("save"), ButtonData.OKDone)
@@ -31,16 +31,14 @@ class PumpDialog(conf: Config, pump: Pump) extends Dialog[Pump] {
   val saveButton = dialog.lookupButton(saveButtonType)
   modelTextField.text.onChange { (_, _, newValue) => saveButton.disable = newValue.trim.isEmpty }
 
-  resultConverter = dialogButton => {
-    if (dialogButton == saveButtonType)
+  resultConverter = dialogButton =>
+    if (dialogButton == saveButtonType) then
       pump.copy(
         installed = installedDatePicker.value.value.toString,
         model = modelTextField.text.value
       )
     else null
-  }
 
   initOwner(App.stage)
   title = conf.getString("title")
   headerText = conf.getString("save-pump") 
-}
