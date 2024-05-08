f50ae1cd2b9800605c0baffd3799845d7b7eee78
objektwerks <objektwerks@runbox.com>
2024-05-08 07:44:26 -0400

    reformatted

--- src/main/scala/poolmate/dialog/HeaterDialog.scala
+++ src/main/scala/poolmate/dialog/HeaterDialog.scala
@@ -2,26 +2,26 @@ package poolmate.dialog
 
 import com.typesafe.config.Config
 
-import poolmate.{App, Entity, Heater}
-import poolmate.pane.ControlGridPane
-
-import scalafx.Includes._
+import scalafx.Includes.*
 import scalafx.scene.control.ButtonBar.ButtonData
 import scalafx.scene.control.{ButtonType, DatePicker, Dialog, TextField}
 import scalafx.scene.layout.Region
 
-class HeaterDialog(conf: Config, heater: Heater) extends Dialog[Heater] {
-  val installedDatePicker = new DatePicker {
+import poolmate.{App, Entity, Heater}
+import poolmate.pane.ControlGridPane
+
+class HeaterDialog(conf: Config, heater: Heater) extends Dialog[Heater]:
+  val installedDatePicker = new DatePicker:
     value = Entity.toLocalDate(heater.installed)
-  }
-  val modelTextField = new TextField {
+
+  val modelTextField = new TextField:
     text = heater.model
-  }
+
   val controls = List[(String, Region)](
     conf.getString("heater-installed") -> installedDatePicker,
     conf.getString("heater-model") -> modelTextField
   )
-  val controlGridPane = new ControlGridPane(controls)
+  val controlGridPane = ControlGridPane(controls)
 
   val dialog = dialogPane()
   val saveButtonType = new ButtonType(conf.getString("save"), ButtonData.OKDone)
@@ -31,16 +31,14 @@ class HeaterDialog(conf: Config, heater: Heater) extends Dialog[Heater] {
   val saveButton = dialog.lookupButton(saveButtonType)
   modelTextField.text.onChange { (_, _, newValue) => saveButton.disable = newValue.trim.isEmpty }
 
-  resultConverter = dialogButton => {
-    if (dialogButton == saveButtonType)
+  resultConverter = dialogButton =>
+    if (dialogButton == saveButtonType) then
       heater.copy(
         installed = installedDatePicker.value.value.toString,
         model = modelTextField.text.value
       )
     else null
-  }
 
   initOwner(App.stage)
   title = conf.getString("title")
   headerText = conf.getString("save-heater")
-}
