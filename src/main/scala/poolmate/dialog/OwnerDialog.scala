63715a83494374f7e8bf8b21660519d65e6b8716
objektwerks <objektwerks@runbox.com>
2024-05-08 07:56:27 -0400

    reformatted

--- src/main/scala/poolmate/dialog/OwnerDialog.scala
+++ src/main/scala/poolmate/dialog/OwnerDialog.scala
@@ -2,34 +2,34 @@ package poolmate.dialog
 
 import com.typesafe.config.Config
 
-import poolmate.{App, Entity, Owner}
-import poolmate.pane.ControlGridPane
-
 import scalafx.Includes._
 import scalafx.scene.control.ButtonBar.ButtonData
 import scalafx.scene.control.{ButtonType, DatePicker, Dialog, TextField}
 import scalafx.scene.layout.Region
 
-class OwnerDialog(conf: Config, owner: Owner) extends Dialog[Owner] {
-  val sinceDatePicker = new DatePicker {
+import poolmate.{App, Entity, Owner}
+import poolmate.pane.ControlGridPane
+
+class OwnerDialog(conf: Config, owner: Owner) extends Dialog[Owner]:
+  val sinceDatePicker = new DatePicker:
     value = Entity.toLocalDate(owner.since)
-  }
-  val firstTextField = new TextField {
+
+  val firstTextField = new TextField:
     text = owner.first
-  }
-  val lastTextField = new TextField {
+
+  val lastTextField = new TextField:
     text = owner.last
-  }
-  val emailTextField = new TextField {
+
+  val emailTextField = new TextField:
     text = owner.email
-  }
+
   val controls = List[(String, Region)](
     conf.getString("owner-since") -> sinceDatePicker,
     conf.getString("owner-first") -> firstTextField,
     conf.getString("owner-last") -> lastTextField,
     conf.getString("owner-email") -> emailTextField
   )
-  val controlGridPane = new ControlGridPane(controls)
+  val controlGridPane = ControlGridPane(controls)
 
   val dialog = dialogPane()
   val saveButtonType = new ButtonType(conf.getString("save"), ButtonData.OKDone)
@@ -41,16 +41,14 @@ class OwnerDialog(conf: Config, owner: Owner) extends Dialog[Owner] {
   lastTextField.text.onChange { (_, _, newValue) => saveButton.disable = newValue.trim.isEmpty }
   emailTextField.text.onChange { (_, _, newValue) => saveButton.disable = newValue.trim.isEmpty }
 
-  resultConverter = dialogButton => {
-    if (dialogButton == saveButtonType)
+  resultConverter = dialogButton =>
+    if (dialogButton == saveButtonType) then
       owner.copy(since = sinceDatePicker.value.value.toString,
         first = firstTextField.text.value,
         last = lastTextField.text.value,
         email = emailTextField.text.value)
     else null
-  }
 
   initOwner(App.stage)
   title = conf.getString("title")
   headerText = conf.getString("save-owner") 
-}
