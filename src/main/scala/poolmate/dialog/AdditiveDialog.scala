4e5349e0cdda7c34afff56e05abd7abf6b7277eb
objektwerks <objektwerks@runbox.com>
2024-05-08 07:34:28 -0400

    reformatted

--- src/main/scala/poolmate/dialog/AdditiveDialog.scala
+++ src/main/scala/poolmate/dialog/AdditiveDialog.scala
@@ -2,36 +2,36 @@ package poolmate.dialog
 
 import com.typesafe.config.Config
 
-import poolmate.{Additive, App, Entity, Resources}
-import Dialogs._
-import poolmate.pane.ControlGridPane
-
-import scalafx.Includes._
+import scalafx.Includes.*
 import scalafx.scene.control.ButtonBar.ButtonData
-import scalafx.scene.control._
+import scalafx.scene.control.{ButtonType, ComboBox, Dialog, DatePicker, TextField}
 import scalafx.scene.layout.Region
 
-class AdditiveDialog(conf: Config, additive: Additive) extends Dialog[Additive] {
-  val onDatePicker = new DatePicker {
+import Dialogs.*
+import poolmate.{Additive, App, Entity, Resources}
+import poolmate.pane.ControlGridPane
+
+class AdditiveDialog(conf: Config, additive: Additive) extends Dialog[Additive]:
+  val onDatePicker = new DatePicker:
     value = Entity.toLocalDate(additive.on)
-  }
-  val chemicalTextField = new TextField {
+
+  val chemicalTextField = new TextField:
     text = additive.chemical
-  }
-  val unitComboBox = new ComboBox[String] {
+
+  val unitComboBox = new ComboBox[String]:
     items = Resources.units
     selectionModel().select(additive.unit)
-  }
-  val amountTextField = new TextField {
+
+  val amountTextField = new TextField:
     text = additive.amount.toString
-  }
+
   val controls = List[(String, Region)](
     conf.getString("additive-on") -> onDatePicker,
     conf.getString("additive-chemical") -> chemicalTextField,
     conf.getString("additive-unit") -> unitComboBox,
     conf.getString("additive-amount") -> amountTextField
   )
-  val controlGridPane = new ControlGridPane(controls)
+  val controlGridPane = ControlGridPane(controls)
 
   val dialog = dialogPane()
   val saveButtonType = new ButtonType(conf.getString("save"), ButtonData.OKDone)
@@ -43,16 +43,14 @@ class AdditiveDialog(conf: Config, additive: Additive) extends Dialog[Additive] 
   val saveButton = dialog.lookupButton(saveButtonType)
   chemicalTextField.text.onChange { (_, _, newValue) => saveButton.disable = newValue.trim.isEmpty }
 
-  resultConverter = dialogButton => {
-    if (dialogButton == saveButtonType)
+  resultConverter = dialogButton =>
+    if (dialogButton == saveButtonType) then
       additive.copy(on = onDatePicker.value.value.toString,
         chemical = chemicalTextField.text.value,
         unit = unitComboBox.selectionModel().getSelectedItem,
         amount = amountTextField.text.value.toDouble)
     else null
-  }
 
   initOwner(App.stage)
   title = conf.getString("title")
-  headerText = conf.getString("save-additive") 
-}
+  headerText = conf.getString("save-additive")
