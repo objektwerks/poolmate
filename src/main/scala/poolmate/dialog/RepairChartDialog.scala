a5e19b40eb25cf3e3f9e5a218df69daa9a2f64b0
objektwerks <objektwerks@runbox.com>
2024-05-08 07:23:01 -0400

    reformatted

--- src/main/scala/poolmate/dialog/RepairChartDialog.scala
+++ src/main/scala/poolmate/dialog/RepairChartDialog.scala
@@ -6,20 +6,23 @@ import java.time.format.DateTimeFormatter
 
 import poolmate.{App, Model}
 
-import scalafx.Includes._
+import scalafx.Includes.*
 import scalafx.geometry.Insets
 import scalafx.scene.chart.{LineChart, NumberAxis, XYChart}
 import scalafx.scene.control.{ButtonType, Dialog}
 import scalafx.scene.layout.VBox
 
-class RepairChartDialog(conf: Config, model: Model) extends Dialog[Unit] {
+class RepairChartDialog(conf: Config, model: Model) extends Dialog[Unit]:
   val repairs = model.repairList
+  
   val dateFormatter = DateTimeFormatter.ofPattern("yy.D")
   val minDate = repairs.map(a => a.on).min.format(dateFormatter).toDouble
   val maxDate = repairs.map(a => a.on).max.format(dateFormatter).toDouble
 
   val xAxis = NumberAxis(axisLabel = s"${conf.getString("repair-chart-year-day")} [$minDate - $maxDate]", lowerBound = minDate, upperBound = maxDate, tickUnit = 1)
+  
   val yAxis = NumberAxis(axisLabel = s"${conf.getString("repair-chart-costs")}", lowerBound = 0.0, upperBound = 1000.00, tickUnit = 100.00)
+  
   val chart = LineChart[Number, Number](xAxis, yAxis)
   chart.padding = Insets(6)
 
@@ -27,9 +30,9 @@ class RepairChartDialog(conf: Config, model: Model) extends Dialog[Unit] {
   val maxCost = repairs.map(r => r.cost).max.toInt
   val avgCost = (repairs.map(r => r.cost).sum / repairs.length).toInt
 
-  val series = new XYChart.Series[Number, Number] {
+  val series = new XYChart.Series[Number, Number]:
     name = s"${conf.getString("min")} $minCost  ${conf.getString("max")} $maxCost  ${conf.getString("avg")} $avgCost"
-  }
+
   repairs foreach { repair => 
     series.data() += XYChart.Data[Number, Number](repair.on.format(dateFormatter).toDouble, repair.cost) 
   }
@@ -37,11 +40,9 @@ class RepairChartDialog(conf: Config, model: Model) extends Dialog[Unit] {
 
   val dialog = dialogPane()
   dialog.buttonTypes = List(ButtonType.Close)
-  dialog.content = new VBox {
+  dialog.content = new VBox:
     spacing = 6; children = List(chart)
-  }
 
   initOwner(App.stage)
   title = conf.getString("repair-chart")
   headerText = conf.getString("repair-costs")
-}
