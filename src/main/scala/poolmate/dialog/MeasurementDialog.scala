ab9b6ee142d4d2fa8585bf807d1e093ca7c55ce7
objektwerks <objektwerks@runbox.com>
2024-05-08 07:55:02 -0400

    reformatted

--- src/main/scala/poolmate/dialog/MeasurementDialog.scala
+++ src/main/scala/poolmate/dialog/MeasurementDialog.scala
@@ -4,100 +4,147 @@ import com.typesafe.config.Config
 
 import java.text.DecimalFormat
 
+import scalafx.Includes.*
+import scalafx.scene.control.ButtonBar.ButtonData
+import scalafx.scene.control.{ButtonType, DatePicker, Dialog, Label, Slider}
+import scalafx.scene.layout.{HBox, Region}
+
 import poolmate.{App, Entity, Measurement}
 import poolmate.pane.ControlGridPane
 
-import scalafx.Includes._
-import scalafx.scene.control.ButtonBar.ButtonData
-import scalafx.scene.control._
-import scalafx.scene.layout.{HBox, Region}
-
-class MeasurementDialog(conf: Config, measurement: Measurement) extends Dialog[Measurement] {
+class MeasurementDialog(conf: Config, measurement: Measurement) extends Dialog[Measurement]:
   val doubleFormatter = new DecimalFormat("#.00")
 
-  val onDatePicker = new DatePicker {
+  val onDatePicker = new DatePicker:
     value = Entity.toLocalDate(measurement.on)
-  }
 
-  val tempSlider = new Slider {
-    prefWidth = 600; min = 0; max = 100; majorTickUnit = 10; showTickLabels = true; showTickMarks = true; value = measurement.temp
-  }
-  val tempLabel = new Label {
+  val tempSlider = new Slider:
+    prefWidth = 600
+    min = 0
+    max = 100
+    majorTickUnit = 10
+    showTickLabels = true
+    showTickMarks = true
+    value = measurement.temp
+
+  val tempLabel = new Label:
     text = measurement.temp.toString
-  }
-  val tempControl = new HBox {
-    spacing = 3; children = List(tempSlider, tempLabel)
-  }
 
-  val hardnessSlider = new Slider {
-    prefWidth = 600; min = 0; max = 1000; majorTickUnit = 100; showTickLabels = true; showTickMarks = true; value = measurement.hardness
-  }
-  val hardnessLabel = new Label {
+  val tempControl = new HBox:
+    spacing = 3
+    children = List(tempSlider, tempLabel)
+
+  val hardnessSlider = new Slider:
+    prefWidth = 600
+    min = 0
+    max = 1000
+    majorTickUnit = 100
+    showTickLabels = true
+    showTickMarks = true
+    value = measurement.hardness
+
+  val hardnessLabel = new Label:
     text = measurement.hardness.toString
-  }
-  val hardnessControl = new HBox {
-    spacing = 3; children = List(hardnessSlider, hardnessLabel)
-  }
 
-  val totalChlorineSlider = new Slider {
-    prefWidth = 600; min = 0; max = 10; majorTickUnit = 1; showTickLabels = true; showTickMarks = true; value = measurement.totalChlorine
-  }
-  val totalChlorineLabel = new Label {
+  val hardnessControl = new HBox:
+    spacing = 3
+    children = List(hardnessSlider, hardnessLabel)
+
+  val totalChlorineSlider = new Slider:
+    prefWidth = 600
+    min = 0
+    max = 10
+    majorTickUnit = 1
+    showTickLabels = true
+    showTickMarks = true
+    value = measurement.totalChlorine
+
+  val totalChlorineLabel = new Label:
     text = measurement.totalChlorine.toString
-  }
-  val totalChlorineControl = new HBox {
-    spacing = 3; children = List(totalChlorineSlider, totalChlorineLabel)
-  }
 
-  val bromineSlider = new Slider {
-    prefWidth = 600; min = 0.0; max = 20; majorTickUnit = 1; showTickLabels = true; showTickMarks = true; value = measurement.bromine
-  }
-  val bromineLabel = new Label {
+  val totalChlorineControl = new HBox:
+    spacing = 3
+    children = List(totalChlorineSlider, totalChlorineLabel)
+
+  val bromineSlider = new Slider:
+    prefWidth = 600
+    min = 0.0
+    max = 20
+    majorTickUnit = 1
+    showTickLabels = true
+    showTickMarks = true
+    value = measurement.bromine
+
+  val bromineLabel = new Label:
     text = measurement.bromine.toString
-  }
-  val bromineControl = new HBox {
-    spacing = 3; children = List(bromineSlider, bromineLabel)
-  }
 
-  val freeChlorineSlider = new Slider {
-    prefWidth = 600; min = 0; max = 10; majorTickUnit = 1; showTickLabels = true; showTickMarks = true; value = measurement.freeChlorine
-  }
-  val freeChlorineLabel = new Label {
+  val bromineControl = new HBox:
+    spacing = 3
+    children = List(bromineSlider, bromineLabel)
+
+  val freeChlorineSlider = new Slider:
+    prefWidth = 600
+    min = 0
+    max = 10
+    majorTickUnit = 1
+    showTickLabels = true
+    showTickMarks = true
+    value = measurement.freeChlorine
+
+  val freeChlorineLabel = new Label:
     text = measurement.freeChlorine.toString
-  }
-  val freeChlorineControl = new HBox {
-    spacing = 3; children = List(freeChlorineSlider, freeChlorineLabel)
-  }
 
-  val phSlider = new Slider {
-    prefWidth = 600; min = 6.2; max = 8.4; majorTickUnit = 0.2; showTickLabels = true; showTickMarks = true; value = measurement.pH
-  }
-  val phLabel = new Label {
+  val freeChlorineControl = new HBox:
+    spacing = 3
+    children = List(freeChlorineSlider, freeChlorineLabel)
+
+  val phSlider = new Slider:
+    prefWidth = 600
+    min = 6.2
+    max = 8.4
+    majorTickUnit = 0.2
+    showTickLabels = true
+    showTickMarks = true
+    value = measurement.pH
+
+  val phLabel = new Label:
     text = doubleFormatter.format(measurement.pH)
-  }
-  val phControl = new HBox {
-    spacing = 3; children = List(phSlider, phLabel)
-  }
 
-  val alkalinitySlider = new Slider {
-    prefWidth = 600; min = 0; max = 240; majorTickUnit = 20; showTickMarks = true; showTickLabels = true; value = measurement.alkalinity
-  }
-  val alkalinityLabel = new Label {
+  val phControl = new HBox:
+    spacing = 3
+    children = List(phSlider, phLabel)
+
+  val alkalinitySlider = new Slider:
+    prefWidth = 600
+    min = 0
+    max = 240
+    majorTickUnit = 20
+    showTickMarks = true
+    showTickLabels = true
+    value = measurement.alkalinity
+
+  val alkalinityLabel = new Label:
     text = measurement.alkalinity.toString
-  }
-  val alkalinityControl = new HBox {
-    spacing = 3; children = List(alkalinitySlider, alkalinityLabel)
-  }
 
-  val cyanuricAcidSlider = new Slider {
-    prefWidth = 600; min = 0; max = 300; majorTickUnit = 25; showTickLabels = true; showTickMarks = true; value = measurement.cyanuricAcid
-  }
-  val cyanuricAcidLabel = new Label {
+  val alkalinityControl = new HBox:
+    spacing = 3
+    children = List(alkalinitySlider, alkalinityLabel)
+
+  val cyanuricAcidSlider = new Slider:
+    prefWidth = 600
+    min = 0
+    max = 300
+    majorTickUnit = 25
+    showTickLabels = true
+    showTickMarks = true
+    value = measurement.cyanuricAcid
+
+  val cyanuricAcidLabel = new Label:
     text = measurement.temp.toString
-  }
-  val cyanuricAcidControl = new HBox {
-    spacing = 3; children = List(cyanuricAcidSlider, cyanuricAcidLabel)
-  }
+
+  val cyanuricAcidControl = new HBox:
+    spacing = 3
+    children = List(cyanuricAcidSlider, cyanuricAcidLabel)
 
   val controls = List[(String, Region)](
     conf.getString("measurement-on") -> onDatePicker,
@@ -110,7 +157,7 @@ class MeasurementDialog(conf: Config, measurement: Measurement) extends Dialog[M
     conf.getString("measurement-alkalinity") -> alkalinityControl,
     conf.getString("measurement-cyanuric-acid") -> cyanuricAcidControl
   )
-  val controlGridPane = new ControlGridPane(controls)
+  val controlGridPane = ControlGridPane(controls)
 
   val dialog = dialogPane()
   val saveButtonType = new ButtonType(conf.getString("save"), ButtonData.OKDone)
@@ -127,8 +174,8 @@ class MeasurementDialog(conf: Config, measurement: Measurement) extends Dialog[M
   cyanuricAcidSlider.value.onChange { (_, _, newValue) => cyanuricAcidLabel.text = newValue.intValue.toString }
 
   // Not usded! Why? val saveButton = dialog.lookupButton(saveButtonType)
-  resultConverter = dialogButton => {
-    if (dialogButton == saveButtonType)
+  resultConverter = dialogButton =>
+    if (dialogButton == saveButtonType) then
       measurement.copy(on = onDatePicker.value.value.toString,
         temp = tempSlider.value.get,
         hardness = hardnessSlider.value.get,
@@ -139,9 +186,7 @@ class MeasurementDialog(conf: Config, measurement: Measurement) extends Dialog[M
         alkalinity = alkalinitySlider.value.get,
         cyanuricAcid = cyanuricAcidSlider.value.get)
     else null
-  }
 
   initOwner(App.stage)
   title = conf.getString("title")
   headerText = conf.getString("save-measurement")
-}
