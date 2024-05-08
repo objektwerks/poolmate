585419180f7c5822d9c93489680ab6587b7ed5b2
objektwerks <objektwerks@runbox.com>
2024-05-08 08:03:56 -0400

    reformatted

--- src/main/scala/poolmate/dialog/SupplyDialog.scala
+++ src/main/scala/poolmate/dialog/SupplyDialog.scala
@@ -2,32 +2,32 @@ package poolmate.dialog
 
 import com.typesafe.config.Config
 
-import poolmate.{App, Entity, Resources, Supply}
-import Dialogs._
-import poolmate.pane.ControlGridPane
-
-import scalafx.Includes._
+import scalafx.Includes.*
 import scalafx.scene.control.ButtonBar.ButtonData
-import scalafx.scene.control._
+import scalafx.scene.control.{ButtonType, ComboBox, Dialog, DatePicker, TextField}
 import scalafx.scene.layout.Region
 
-class SupplyDialog(conf: Config, supply: Supply) extends Dialog[Supply] {
-  val purchasedDatePicker = new DatePicker {
+import Dialogs.*
+import poolmate.{App, Entity, Resources, Supply}
+import poolmate.pane.ControlGridPane
+
+class SupplyDialog(conf: Config, supply: Supply) extends Dialog[Supply]:
+  val purchasedDatePicker = new DatePicker:
     value = Entity.toLocalDate(supply.purchased)
-  }
-  val itemTextField = new TextField {
+
+  val itemTextField = new TextField:
     text = supply.item
-  }
-  val unitComboBox = new ComboBox[String] {
+
+  val unitComboBox = new ComboBox[String]:
     items = Resources.units
     selectionModel().select(supply.unit)
-  }
-  val amountTextField = new TextField {
+
+  val amountTextField = new TextField:
     text = supply.amount.toString
-  }
-  val costTextField = new TextField {
+
+  val costTextField = new TextField:
     text = supply.cost.toString
-  }
+
   val controls = List[(String, Region)](
     conf.getString("supply-purchased") -> purchasedDatePicker,
     conf.getString("supply-item") -> itemTextField,
@@ -35,7 +35,7 @@ class SupplyDialog(conf: Config, supply: Supply) extends Dialog[Supply] {
     conf.getString("supply-amount") -> amountTextField,
     conf.getString("supply-cost") -> costTextField
   )
-  val controlGridPane = new ControlGridPane(controls)
+  val controlGridPane = ControlGridPane(controls)
 
   val dialog = dialogPane()
   val saveButtonType = new ButtonType(conf.getString("save"), ButtonData.OKDone)
@@ -43,12 +43,12 @@ class SupplyDialog(conf: Config, supply: Supply) extends Dialog[Supply] {
   dialog.content = controlGridPane
 
   val saveButton = dialog.lookupButton(saveButtonType)
-  amountTextField.text.onChange { (_, oldValue, newValue) => if (isNotNumeric(newValue)) amountTextField.text.value = oldValue }
-  costTextField.text.onChange { (_, oldValue, newValue) => if (isNotNumeric(newValue)) costTextField.text.value = oldValue }
+  amountTextField.text.onChange { (_, oldValue, newValue) => if (isNotNumeric(newValue)) then amountTextField.text.value = oldValue }
+  costTextField.text.onChange { (_, oldValue, newValue) => if (isNotNumeric(newValue)) then costTextField.text.value = oldValue }
   itemTextField.text.onChange { (_, _, newValue) => saveButton.disable = newValue.trim.isEmpty }
 
-  resultConverter = dialogButton => {
-    if (dialogButton == saveButtonType)
+  resultConverter = dialogButton =>
+    if (dialogButton == saveButtonType) then
       supply.copy(
         purchased = purchasedDatePicker.value.value.toString,
         item = itemTextField.text.value,
@@ -57,9 +57,7 @@ class SupplyDialog(conf: Config, supply: Supply) extends Dialog[Supply] {
         cost = costTextField.text.value.toDouble
       )
     else null
-  }
 
   initOwner(App.stage)
   title = conf.getString("title")
   headerText = conf.getString("save-supply") 
-}
