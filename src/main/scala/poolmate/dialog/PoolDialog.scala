76459eccfa1672bd8ac164c0863e7e07369cbe11
objektwerks <objektwerks@runbox.com>
2024-05-09 06:40:01 -0400

    reformatted

--- src/main/scala/poolmate/dialog/PoolDialog.scala
+++ src/main/scala/poolmate/dialog/PoolDialog.scala
@@ -41,7 +41,7 @@ class PoolDialog(conf: Config, pool: Pool) extends Dialog[Pool]:
   val controlGridPane = ControlGridPane(controls)
 
   val dialog = dialogPane()
-  val saveButtonType = new ButtonType(conf.getString("save"), ButtonData.OKDone)
+  val saveButtonType = ButtonType(conf.getString("save"), ButtonData.OKDone)
   dialog.buttonTypes = List(saveButtonType, ButtonType.Cancel)
   dialog.content = controlGridPane
 
