0562e6d177ad071e400279999a57758d0de44835
objektwerks <objektwerks@runbox.com>
2024-05-09 07:14:42 -0400

    reformatted

--- src/main/scala/poolmate/pane/PoolPane.scala
+++ src/main/scala/poolmate/pane/PoolPane.scala
@@ -2,56 +2,58 @@ package poolmate.pane
 
 import com.typesafe.config.Config
 
-import poolmate.{Model, Pool}
-import poolmate.Resources._
-import poolmate.dialog.PoolDialog
-
-import scalafx.Includes._
+import scalafx.Includes.*
 import scalafx.geometry.Insets
-import scalafx.scene.control.TableColumn._
-import scalafx.scene.control._
+import scalafx.scene.control.{Button, Label, SelectionMode, TableColumn, TableView}
 import scalafx.scene.layout.{HBox, VBox}
 
-class PoolPane(conf: Config, model: Model) extends VBox {
-  val poolLabel = new Label {
+import poolmate.{Model, Pool}
+import poolmate.Resources.*
+import poolmate.dialog.PoolDialog
+
+class PoolPane(conf: Config, model: Model) extends VBox:
+  val poolLabel = new Label:
     text = conf.getString("pools")
-  }
-  val poolTableView = new TableView[Pool]() {
+
+  val poolTableView = new TableView[Pool]:
     columns ++= List(
-      new TableColumn[Pool, String] {
-        text = conf.getString("pool-header-built"); cellValueFactory = {
+      new TableColumn[Pool, String]:
+        text = conf.getString("pool-header-built")
+        cellValueFactory = {
           _.value.builtProperty
         }
-      },
-      new TableColumn[Pool, String] {
-        text = conf.getString("pool-header-gallons"); cellValueFactory = {
+      ,
+      new TableColumn[Pool, String]:
+        text = conf.getString("pool-header-gallons")
+        cellValueFactory = {
           _.value.gallonsProperty
         }
-      },
-      new TableColumn[Pool, String] {
-        text = conf.getString("pool-header-street"); cellValueFactory = {
+      ,
+      new TableColumn[Pool, String]:
+        text = conf.getString("pool-header-street")
+        cellValueFactory = {
           _.value.streetProperty
         }
-      },
-      new TableColumn[Pool, String] {
-        text = conf.getString("pool-header-city"); cellValueFactory = {
+      ,
+      new TableColumn[Pool, String]:
+        text = conf.getString("pool-header-city")
+        cellValueFactory = {
           _.value.cityProperty
         }
-      }
     )
     items = model.poolList
-  }
   poolTableView.selectionModel().selectionModeProperty.value = SelectionMode.Single
-  val poolAddButton = new Button {
+
+  val poolAddButton = new Button:
     graphic = addImageView
-  }
-  val poolEditButton = new Button {
+
+  val poolEditButton = new Button:
     graphic = editImageView
     disable = true
-  }
-  val poolToolBar = new HBox {
-    spacing = 6; children = List(poolAddButton, poolEditButton)
-  }
+
+  val poolToolBar = new HBox:
+    spacing = 6
+    children = List(poolAddButton, poolEditButton)
 
   spacing = 6
   padding = Insets(6)
@@ -59,37 +61,37 @@ class PoolPane(conf: Config, model: Model) extends VBox {
 
   poolTableView.selectionModel().selectedItemProperty().addListener { (_, _, selectedPool) =>
     // model.update executes a remove and add on items. the remove passes a null selectedPool!
-    if (selectedPool != null) {
+    if (selectedPool != null) then
       model.selectedPoolId.value = selectedPool.id
       poolEditButton.disable = false
-    }
   }
 
   poolTableView.onMouseClicked = { event =>
-    if (event.getClickCount == 2 && poolTableView.selectionModel().getSelectedItem != null) update()
+    if (event.getClickCount == 2 &&
+        poolTableView.selectionModel().getSelectedItem != null) then update()
   }
 
   poolAddButton.onAction = { _ => add() }
 
   poolEditButton.onAction = { _ => update() }
 
-  def add(): Unit = {
-    new PoolDialog(conf, Pool()).showAndWait() match {
+  def add(): Unit =
+    new PoolDialog(conf, Pool()).showAndWait() match 
       case Some(Pool(id, built, gallons, street, city, state, zip)) =>
-        val newPool = model.addPool(Pool(id, built, gallons, street, city, state, zip))
+        val newPool = model.addPool(
+          Pool(id, built, gallons, street, city, state, zip)
+        )
         poolTableView.selectionModel().select(newPool)
       case _ =>
-    }
-  }
 
-  def update(): Unit = {
+  def update(): Unit = 
     val selectedIndex = poolTableView.selectionModel().getSelectedIndex
     val pool = poolTableView.selectionModel().getSelectedItem.pool
-    new PoolDialog(conf, pool).showAndWait() match {
+    PoolDialog(conf, pool).showAndWait() match
       case Some(Pool(id, built, gallons, street, city, state, zip)) =>
-        model.updatePool(selectedIndex, Pool(id, built, gallons, street, city, state, zip))
+        model.updatePool(
+          selectedIndex,
+          Pool(id, built, gallons, street, city, state, zip)
+        )
         poolTableView.selectionModel().select(selectedIndex)
       case _ =>
-    }
-  }
-}
