438f3c65a8a4869323d7d6b136324f0a1b289c4e
objektwerks <objektwerks@runbox.com>
2024-05-09 07:07:29 -0400

    reformatted

--- src/main/scala/poolmate/pane/MenuPane.scala
+++ src/main/scala/poolmate/pane/MenuPane.scala
@@ -2,36 +2,29 @@ package poolmate.pane
 
 import com.typesafe.config.Config
 
-import poolmate.App
-
 import scalafx.application.Platform
 import scalafx.scene.control.Alert.AlertType
-import scalafx.scene.control._
+import scalafx.scene.control.{Alert, Menu, MenuBar, MenuItem, SeparatorMenuItem}
 
-class MenuPane(conf: Config) extends MenuBar {
-  val aboutDialog = new Alert(AlertType.Information) {
+import poolmate.App
+
+class MenuPane(conf: Config) extends MenuBar:
+  val aboutDialog = new Alert(AlertType.Information):
     initOwner(App.stage)
     title = conf.getString("about")
     headerText = conf.getString("developer")
     contentText = s"${conf.getString("app")} ${conf.getString("license")}"
-  }
 
-  val aboutMenuItem = new MenuItem(conf.getString("about")) {
-    onAction = { _ =>
-      aboutDialog.showAndWait()
-      ()
-    }
-  }
-  val separator = new SeparatorMenuItem()
+  val aboutMenuItem = new MenuItem(conf.getString("about")):
+    onAction = { _ => aboutDialog.showAndWait() }
 
-  val exitMenuItem = new MenuItem(conf.getString("exit")) {
+  val separator = SeparatorMenuItem()
+
+  val exitMenuItem = new MenuItem(conf.getString("exit")):
     onAction = { _ => Platform.exit() }
-  }
   
-  val menu = new Menu(conf.getString("menu")) {
+  val menu = new Menu(conf.getString("menu")):
     items = List(aboutMenuItem, separator, exitMenuItem)
-  }
 
   menus = List(menu)
   useSystemMenuBar = false
-}
