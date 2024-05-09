a67f21144cf064910d2a1ca417c8ed17ec1deda1
objektwerks <objektwerks@runbox.com>
2024-05-09 07:20:56 -0400

    reformatted

--- src/main/scala/poolmate/pane/PumpPane.scala
+++ src/main/scala/poolmate/pane/PumpPane.scala
@@ -2,44 +2,44 @@ package poolmate.pane
 
 import com.typesafe.config.Config
 
-import poolmate.{Model, Pump}
-import poolmate.Resources._
-import poolmate.dialog.PumpDialog
-
-import scalafx.Includes._
+import scalafx.Includes.*
 import scalafx.geometry.Insets
-import scalafx.scene.control.TableColumn._
-import scalafx.scene.control._
+import scalafx.scene.control.{Button, SelectionMode, TableColumn, TableView}
 import scalafx.scene.layout.{HBox, VBox}
 
-class PumpPane(conf: Config, model: Model) extends VBox {
-  val pumpTableView = new TableView[Pump]() {
+import poolmate.{Model, Pump}
+import poolmate.Resources.*
+import poolmate.dialog.PumpDialog
+
+class PumpPane(conf: Config, model: Model) extends VBox:
+  val pumpTableView = new TableView[Pump]:
     columns ++= List(
-      new TableColumn[Pump, String] {
-        text = conf.getString("pump-header-installed"); cellValueFactory = {
+      new TableColumn[Pump, String]:
+        text = conf.getString("pump-header-installed")
+        cellValueFactory = {
           _.value.installedProperty
         }
-      },
-      new TableColumn[Pump, String] {
-        text = conf.getString("pump-header-model"); cellValueFactory = {
+      ,
+      new TableColumn[Pump, String]:
+        text = conf.getString("pump-header-model")
+        cellValueFactory = {
           _.value.modelProperty
         }
-      }
     )
     items = model.pumpList
-  }
   pumpTableView.selectionModel().selectionModeProperty.value = SelectionMode.Single
-  val pumpAddButton = new Button {
+
+  val pumpAddButton = new Button:
     graphic = addImageView
     disable = true
-  }
-  val pumpEditButton = new Button {
+
+  val pumpEditButton = new Button:
     graphic = editImageView
     disable = true
-  }
-  val pumpToolBar = new HBox {
-    spacing = 6; children = List(pumpAddButton, pumpEditButton)
-  }
+
+  val pumpToolBar = new HBox:
+    spacing = 6
+    children = List(pumpAddButton, pumpEditButton)
 
   spacing = 6
   padding = Insets(6)
@@ -52,37 +52,37 @@ class PumpPane(conf: Config, model: Model) extends VBox {
 
   pumpTableView.selectionModel().selectedItemProperty().addListener { (_, _, selectedPump) =>
     // model.update executes a remove and add on items. the remove passes a null selectedPump!
-    if (selectedPump != null) {
+    if (selectedPump != null) then
       model.selectedPumpId.value = selectedPump.id
       pumpEditButton.disable = false
-    }
   }
 
   pumpTableView.onMouseClicked = { event =>
-    if (event.getClickCount == 2 && pumpTableView.selectionModel().getSelectedItem != null) update()
+    if (event.getClickCount == 2 &&
+        pumpTableView.selectionModel().getSelectedItem != null) then update()
   }
 
   pumpAddButton.onAction = { _ => add() }
 
   pumpEditButton.onAction = { _ => update() }
 
-  def add(): Unit = {
-    new PumpDialog(conf, Pump(poolId = model.selectedPoolId.toInt)).showAndWait() match {
+  def add(): Unit = 
+    PumpDialog(conf, Pump(poolId = model.selectedPoolId.toInt)).showAndWait() match
       case Some(Pump(id, poolId, installed, _model)) =>
-        val newPump = model.addPump(Pump(id, poolId, installed, _model))
+        val newPump = model.addPump(
+          Pump(id, poolId, installed, _model)
+        )
         pumpTableView.selectionModel().select(newPump)
       case _ =>
-    }
-  }
 
-  def update(): Unit = {
+  def update(): Unit =
     val selectedIndex = pumpTableView.selectionModel().getSelectedIndex
     val pump = pumpTableView.selectionModel().getSelectedItem.pump
-    new PumpDialog(conf, pump).showAndWait() match {
+    PumpDialog(conf, pump).showAndWait() match
       case Some(Pump(id, poolId, installed, _model)) =>
-        model.updatePump(selectedIndex, Pump(id, poolId, installed, _model))
+        model.updatePump(
+          selectedIndex,
+          Pump(id, poolId, installed, _model)
+        )
         pumpTableView.selectionModel().select(selectedIndex)
       case _ =>
-    }
-  }
-}
