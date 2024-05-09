fbd08933c32fd99935cd44cbceb221c076e5dbd4
objektwerks <objektwerks@runbox.com>
2024-05-09 06:37:14 -0400

    reformatted

--- src/main/scala/poolmate/dialog/CleaningDialog.scala
+++ src/main/scala/poolmate/dialog/CleaningDialog.scala
@@ -48,7 +48,7 @@ class CleaningDialog(conf: Config, cleaning: Cleaning) extends Dialog[Cleaning]:
   val controlGridPane = ControlGridPane(controls)
 
   val dialog = dialogPane()
-  val saveButtonType = new ButtonType(conf.getString("save"), ButtonData.OKDone)
+  val saveButtonType = ButtonType(conf.getString("save"), ButtonData.OKDone)
   dialog.buttonTypes = List(saveButtonType, ButtonType.Cancel)
   dialog.content = controlGridPane
 
