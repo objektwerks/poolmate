54ccc08d5ad95a3d25cb8294c2650510bd827438
objektwerks <objektwerks@runbox.com>
2024-05-09 06:13:25 -0400

    reformatted

--- src/main/scala/poolmate/pane/CleaningPane.scala
+++ src/main/scala/poolmate/pane/CleaningPane.scala
@@ -2,75 +2,79 @@ package poolmate.pane
 
 import com.typesafe.config.Config
 
-import poolmate.{Cleaning, Model}
-import poolmate.Resources._
-import poolmate.dialog.CleaningDialog
-
-import scalafx.Includes._
+import scalafx.Includes.*
 import scalafx.geometry.Insets
-import scalafx.scene.control.TableColumn._
-import scalafx.scene.control._
+import scalafx.scene.control.{Button, SelectionMode, TableColumn, TableView}
 import scalafx.scene.layout.{HBox, VBox}
 
-class CleaningPane(conf: Config, model: Model) extends VBox {
-  val cleaningTableView = new TableView[Cleaning]() {
+import poolmate.{Cleaning, Model}
+import poolmate.Resources.*
+import poolmate.dialog.CleaningDialog
+
+class CleaningPane(conf: Config, model: Model) extends VBox:
+  val cleaningTableView = new TableView[Cleaning]:
     columns ++= List(
-      new TableColumn[Cleaning, String] {
-        text = conf.getString("cleaning-header-on"); cellValueFactory = {
+      new TableColumn[Cleaning, String]:
+        text = conf.getString("cleaning-header-on")
+        cellValueFactory = {
           _.value.onProperty
         }
-      },
-      new TableColumn[Cleaning, String] {
-        text = conf.getString("cleaning-header-deck"); cellValueFactory = {
+      ,
+      new TableColumn[Cleaning, String]:
+        text = conf.getString("cleaning-header-deck")
+        cellValueFactory = {
           _.value.deckProperty
         }
-      },
-      new TableColumn[Cleaning, String] {
-        text = conf.getString("cleaning-header-brush"); cellValueFactory = {
+      ,
+      new TableColumn[Cleaning, String]:
+        text = conf.getString("cleaning-header-brush")
+        cellValueFactory = {
           _.value.brushProperty
         }
-      },
-      new TableColumn[Cleaning, String] {
-        text = conf.getString("cleaning-header-net"); cellValueFactory = {
+      ,
+      new TableColumn[Cleaning, String]:
+        text = conf.getString("cleaning-header-net")
+        cellValueFactory = {
           _.value.netProperty
         }
-      },
-      new TableColumn[Cleaning, String] {
-        text = conf.getString("cleaning-header-vacuum"); cellValueFactory = {
+      ,
+      new TableColumn[Cleaning, String]:
+        text = conf.getString("cleaning-header-vacuum")
+        cellValueFactory = {
           _.value.vacuumProperty
         }
-      },
-      new TableColumn[Cleaning, String] {
-        text = conf.getString("cleaning-header-skimmer-basket"); cellValueFactory = {
+      ,
+      new TableColumn[Cleaning, String]:
+        text = conf.getString("cleaning-header-skimmer-basket")
+        cellValueFactory = {
           _.value.skimmerBasketProperty
         }
-      },
-      new TableColumn[Cleaning, String] {
+      ,
+      new TableColumn[Cleaning, String]:
         text = conf.getString("cleaning-header-pump-basket"); cellValueFactory = {
           _.value.pumpBasketProperty
         }
-      },
-      new TableColumn[Cleaning, String] {
-        text = conf.getString("cleaning-header-pump-filter"); cellValueFactory = {
+      ,
+      new TableColumn[Cleaning, String]:
+        text = conf.getString("cleaning-header-pump-filter")
+        cellValueFactory = {
           _.value.pumpFilterProperty
         }
-      }
     )
     prefHeight = conf.getInt("height").toDouble
     items = model.cleaningList
-  }
   cleaningTableView.selectionModel().selectionModeProperty.value = SelectionMode.Single
-  val cleaningAddButton = new Button {
+
+  val cleaningAddButton = new Button:
     graphic = addImageView
     disable = true
-  }
-  val cleaningEditButton = new Button {
+
+  val cleaningEditButton = new Button:
     graphic = editImageView
     disable = true
-  }
-  val cleaningToolBar = new HBox {
+
+  val cleaningToolBar = new HBox:
     spacing = 6; children = List(cleaningAddButton, cleaningEditButton)
-  }
 
   spacing = 6
   padding = Insets(6)
@@ -83,37 +87,32 @@ class CleaningPane(conf: Config, model: Model) extends VBox {
 
   cleaningTableView.selectionModel().selectedItemProperty().addListener { (_, _, selectedCleaning) =>
     // model.update executes a remove and add on items. the remove passes a null selectedCleaning!
-    if (selectedCleaning != null) {
+    if (selectedCleaning != null) then
       model.selectedCleaningId.value = selectedCleaning.id
       cleaningEditButton.disable = false
-    }
   }
 
   cleaningTableView.onMouseClicked = { event =>
-    if (event.getClickCount == 2 && cleaningTableView.selectionModel().getSelectedItem != null) update()
+    if (event.getClickCount == 2 &&
+        cleaningTableView.selectionModel().getSelectedItem != null) then update()
   }
 
   cleaningAddButton.onAction = { _ => add() }
 
   cleaningEditButton.onAction = { _ => update() }
 
-  def add(): Unit = {
-    new CleaningDialog(conf, Cleaning(poolId = model.selectedPoolId.toInt)).showAndWait() match {
+  def add(): Unit =
+    CleaningDialog(conf, Cleaning(poolId = model.selectedPoolId.toInt)).showAndWait() match
       case Some(Cleaning(id, poolId, on, deck, brush, net, vacuum, skimmerBasket, pumpBasket, pumpFilter)) =>
         val newCleaning = model.addCleaning(Cleaning(id, poolId, on, deck, brush, net, vacuum, skimmerBasket, pumpBasket, pumpFilter))
         cleaningTableView.selectionModel().select(newCleaning)
       case _ =>
-    }
-  }
 
-  def update(): Unit = {
+  def update(): Unit =
     val selectedIndex = cleaningTableView.selectionModel().getSelectedIndex
     val cleaning = cleaningTableView.selectionModel().getSelectedItem.cleaning
-    new CleaningDialog(conf, cleaning).showAndWait() match {
+    CleaningDialog(conf, cleaning).showAndWait() match
       case Some(Cleaning(id, poolId, on, deck, brush, net, vacuum, skimmerBasket, pumpBasket, pumpFilter)) =>
         model.updateCleaning(selectedIndex, Cleaning(id, poolId, on, deck, brush, net, vacuum, skimmerBasket, pumpBasket, pumpFilter))
         cleaningTableView.selectionModel().select(selectedIndex)
       case _ =>
-    }
-  }
-}
