18a3b0f13e290a96f80278816c5bd1134b5b0b63
objektwerks <objektwerks@runbox.com>
2024-05-09 06:55:34 -0400

    reformatted

--- src/main/scala/poolmate/pane/HeaterPane.scala
+++ src/main/scala/poolmate/pane/HeaterPane.scala
@@ -3,43 +3,42 @@ package poolmate.pane
 import com.typesafe.config.Config
 
 import poolmate.{Heater, Model}
-import poolmate.Resources._
+import poolmate.Resources.*
 import poolmate.dialog.HeaterDialog
 
-import scalafx.Includes._
+import scalafx.Includes.*
 import scalafx.geometry.Insets
-import scalafx.scene.control.TableColumn._
-import scalafx.scene.control._
+import scalafx.scene.control.{Button, SelectionMode, TableColumn, TableView}
 import scalafx.scene.layout.{HBox, VBox}
 
-class HeaterPane(conf: Config, model: Model) extends VBox {
-  val heaterTableView = new TableView[Heater]() {
+class HeaterPane(conf: Config, model: Model) extends VBox:
+  val heaterTableView = new TableView[Heater]:
     columns ++= List(
-      new TableColumn[Heater, String] {
-        text = conf.getString("heater-header-installed"); cellValueFactory = {
+      new TableColumn[Heater, String]:
+        text = conf.getString("heater-header-installed")
+        cellValueFactory = {
           _.value.installedProperty
         }
-      },
-      new TableColumn[Heater, String] {
-        text = conf.getString("heater-header-model"); cellValueFactory = {
+      ,
+      new TableColumn[Heater, String]:
+        text = conf.getString("heater-header-model")
+        cellValueFactory = {
           _.value.modelProperty
         }
-      }
     )
     items = model.heaterList
-  }
   heaterTableView.selectionModel().selectionModeProperty.value = SelectionMode.Single
-  val heaterAddButton = new Button {
+
+  val heaterAddButton = new Button:
     graphic = addImageView
     disable = true
-  }
-  val heaterEditButton = new Button {
+
+  val heaterEditButton = new Button:
     graphic = editImageView
     disable = true
-  }
-  val heaterToolBar = new HBox {
+
+  val heaterToolBar = new HBox:
     spacing = 6; children = List(heaterAddButton, heaterEditButton)
-  }
 
   spacing = 6
   padding = Insets(6)
@@ -52,37 +51,32 @@ class HeaterPane(conf: Config, model: Model) extends VBox {
 
   heaterTableView.selectionModel().selectedItemProperty().addListener { (_, _, selectedHeater) =>
     // model.update executes a remove and add on items. the remove passes a null selectedHeater!
-    if (selectedHeater != null) {
+    if (selectedHeater != null) then
       model.selectedHeaterId.value = selectedHeater.id
       heaterEditButton.disable = false
-    }
   }
 
   heaterTableView.onMouseClicked = { event =>
-    if (event.getClickCount == 2 && heaterTableView.selectionModel().getSelectedItem != null) update()
+    if (event.getClickCount == 2 &&
+        heaterTableView.selectionModel().getSelectedItem != null) then update()
   }
 
   heaterAddButton.onAction = { _ => add() }
 
   heaterEditButton.onAction = { _ => update() }
 
-  def add(): Unit = {
-    new HeaterDialog(conf, Heater(poolId = model.selectedPoolId.toInt)).showAndWait() match {
+  def add(): Unit =
+    HeaterDialog(conf, Heater(poolId = model.selectedPoolId.toInt)).showAndWait() match
       case Some(Heater(id, poolId, installed, _model)) =>
         val newHeater = model.addHeater(Heater(id, poolId, installed, _model))
         heaterTableView.selectionModel().select(newHeater)
       case _ =>
-    }
-  }
 
-  def update(): Unit = {
+  def update(): Unit =
     val selectedIndex = heaterTableView.selectionModel().getSelectedIndex
     val heater = heaterTableView.selectionModel().getSelectedItem.heater
-    new HeaterDialog(conf, heater).showAndWait() match {
+    HeaterDialog(conf, heater).showAndWait() match
       case Some(Heater(id, poolId, installed, _model)) =>
         model.updateHeater(selectedIndex, Heater(id, poolId, installed, _model))
         heaterTableView.selectionModel().select(selectedIndex)
       case _ =>
-    }
-  }
-}
