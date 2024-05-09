4b49799a8023adda853193bbbbd868265b3fa070
objektwerks <objektwerks@runbox.com>
2024-05-09 06:39:19 -0400

    reformatted

--- src/main/scala/poolmate/dialog/MeasurementDialog.scala
+++ src/main/scala/poolmate/dialog/MeasurementDialog.scala
@@ -160,7 +160,7 @@ class MeasurementDialog(conf: Config, measurement: Measurement) extends Dialog[M
   val controlGridPane = ControlGridPane(controls)
 
   val dialog = dialogPane()
-  val saveButtonType = new ButtonType(conf.getString("save"), ButtonData.OKDone)
+  val saveButtonType = ButtonType(conf.getString("save"), ButtonData.OKDone)
   dialog.buttonTypes = List(saveButtonType, ButtonType.Cancel)
   dialog.content = controlGridPane
 
