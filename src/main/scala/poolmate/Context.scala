28f4117dc9a2a1054cde479853e6fe9a518c1f4c
objektwerks <objektwerks@runbox.com>
2024-05-16 11:23:39 -0400

    enhanced context

--- src/main/scala/poolmate/Context.scala
+++ src/main/scala/poolmate/Context.scala
@@ -162,6 +162,7 @@ additive-amounts = "Additive Amounts"
   val additives = config.getString("additives")
   val additiveOn = config.getString("additive-on")
   val additiveChemical = config.getString("additive-chemical")
+  val additiveUnit = config.getString("additive-unit")
 
 
   def units = ObservableBuffer[String]( config.getStringList("units").asScala.toSeq * )
