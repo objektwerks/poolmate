16aee58926afefd9ba4fc9a998146f8a44fe5896
objektwerks <objektwerks@runbox.com>
2024-05-17 11:04:02 -0400

    enhanced supply dialog

--- src/main/scala/poolmate/dialog/SupplyDialog.scala
+++ src/main/scala/poolmate/dialog/SupplyDialog.scala
@@ -1,17 +1,15 @@
 package poolmate.dialog
 
-import com.typesafe.config.Config
-
 import scalafx.Includes.*
 import scalafx.scene.control.ButtonBar.ButtonData
 import scalafx.scene.control.{ButtonType, ComboBox, Dialog, DatePicker, TextField}
 import scalafx.scene.layout.Region
 
 import Dialogs.*
-import poolmate.{App, Entity, Context, Supply}
+import poolmate.{App, Context, Entity, Supply}
 import poolmate.pane.ControlGridPane
 
-class SupplyDialog(conf: Config, supply: Supply) extends Dialog[Supply]:
+class SupplyDialog(context: Context, supply: Supply) extends Dialog[Supply]:
   val purchasedDatePicker = new DatePicker:
     value = Entity.toLocalDate(supply.purchased)
 
@@ -19,7 +17,7 @@ class SupplyDialog(conf: Config, supply: Supply) extends Dialog[Supply]:
     text = supply.item
 
   val unitComboBox = new ComboBox[String]:
-    items = Context.units
+    items = context.units
     selectionModel().select(supply.unit)
 
   val amountTextField = new TextField:
@@ -29,16 +27,16 @@ class SupplyDialog(conf: Config, supply: Supply) extends Dialog[Supply]:
     text = supply.cost.toString
 
   val controls = List[(String, Region)](
-    conf.getString("supply-purchased") -> purchasedDatePicker,
-    conf.getString("supply-item") -> itemTextField,
-    conf.getString("supply-unit") -> unitComboBox,
-    conf.getString("supply-amount") -> amountTextField,
-    conf.getString("supply-cost") -> costTextField
+    context.supplyPurchased -> purchasedDatePicker,
+    context.supplyItem -> itemTextField,
+    context.supplyUnit -> unitComboBox,
+    context.supplyAmount -> amountTextField,
+    context.supplyCost -> costTextField
   )
   val controlGridPane = ControlGridPane(controls)
 
   val dialog = dialogPane()
-  val saveButtonType = ButtonType(conf.getString("save"), ButtonData.OKDone)
+  val saveButtonType = ButtonType(context.save, ButtonData.OKDone)
   dialog.buttonTypes = List(saveButtonType, ButtonType.Cancel)
   dialog.content = controlGridPane
 
@@ -59,5 +57,5 @@ class SupplyDialog(conf: Config, supply: Supply) extends Dialog[Supply]:
     else null
 
   initOwner(App.stage)
-  title = conf.getString("title")
-  headerText = conf.getString("save-supply") 
+  title = context.title
+  headerText = context.saveSupply 
