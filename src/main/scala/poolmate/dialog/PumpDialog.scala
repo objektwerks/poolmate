1054ab60ad7fbfdfdfdec3382150cd25e1d82344
objektwerks <objektwerks@runbox.com>
2024-05-17 10:53:26 -0400

    enhanced pump dialog

--- src/main/scala/poolmate/dialog/PumpDialog.scala
+++ src/main/scala/poolmate/dialog/PumpDialog.scala
@@ -1,16 +1,14 @@
 package poolmate.dialog
 
-import com.typesafe.config.Config
-
 import scalafx.Includes.*
 import scalafx.scene.control.ButtonBar.ButtonData
 import scalafx.scene.control.{ButtonType, DatePicker, Dialog, TextField}
 import scalafx.scene.layout.Region
 
-import poolmate.{App, Entity, Pump}
+import poolmate.{App, Context, Entity, Pump}
 import poolmate.pane.ControlGridPane
 
-class PumpDialog(conf: Config, pump: Pump) extends Dialog[Pump]:
+class PumpDialog(context: Context, pump: Pump) extends Dialog[Pump]:
   val installedDatePicker = new DatePicker:
     value = Entity.toLocalDate(pump.installed)
 
@@ -18,13 +16,13 @@ class PumpDialog(conf: Config, pump: Pump) extends Dialog[Pump]:
     text = pump.model
 
   val controls = List[(String, Region)](
-    conf.getString("pump-installed") -> installedDatePicker,
-    conf.getString("pump-model") -> modelTextField
+    context.pumpInstalled -> installedDatePicker,
+    context.pumpModel -> modelTextField
   )
   val controlGridPane = ControlGridPane(controls)
 
   val dialog = dialogPane()
-  val saveButtonType = ButtonType(conf.getString("save"), ButtonData.OKDone)
+  val saveButtonType = ButtonType(context.save, ButtonData.OKDone)
   dialog.buttonTypes = List(saveButtonType, ButtonType.Cancel)
   dialog.content = controlGridPane
 
@@ -40,5 +38,5 @@ class PumpDialog(conf: Config, pump: Pump) extends Dialog[Pump]:
     else null
 
   initOwner(App.stage)
-  title = conf.getString("title")
-  headerText = conf.getString("save-pump") 
+  title = context.title
+  headerText = context.savePump 
