286685707b8038266d338c807347113205611abd
objektwerks <objektwerks@runbox.com>
2024-05-17 10:57:52 -0400

    enhanced reapir chart dialog

--- src/main/scala/poolmate/dialog/RepairChartDialog.scala
+++ src/main/scala/poolmate/dialog/RepairChartDialog.scala
@@ -1,7 +1,5 @@
 package poolmate.dialog
 
-import com.typesafe.config.Config
-
 import java.time.format.DateTimeFormatter
 
 import scalafx.Includes.*
@@ -10,18 +8,18 @@ import scalafx.scene.chart.{LineChart, NumberAxis, XYChart}
 import scalafx.scene.control.{ButtonType, Dialog}
 import scalafx.scene.layout.VBox
 
-import poolmate.{App, Model}
+import poolmate.{App, Context, Model}
 
-class RepairChartDialog(conf: Config, model: Model) extends Dialog[Unit]:
+class RepairChartDialog(context: Context, model: Model) extends Dialog[Unit]:
   val repairs = model.repairList
 
   val dateFormatter = DateTimeFormatter.ofPattern("yy.D")
   val minDate = repairs.map(a => a.on).min.format(dateFormatter).toDouble
   val maxDate = repairs.map(a => a.on).max.format(dateFormatter).toDouble
 
-  val xAxis = NumberAxis(axisLabel = s"${conf.getString("repair-chart-year-day")} [$minDate - $maxDate]", lowerBound = minDate, upperBound = maxDate, tickUnit = 1)
+  val xAxis = NumberAxis(axisLabel = s"${context.repairChartYearDay} [$minDate - $maxDate]", lowerBound = minDate, upperBound = maxDate, tickUnit = 1)
   
-  val yAxis = NumberAxis(axisLabel = s"${conf.getString("repair-chart-costs")}", lowerBound = 0.0, upperBound = 1000.00, tickUnit = 100.00)
+  val yAxis = NumberAxis(axisLabel = s"${context.repairChartCosts}", lowerBound = 0.0, upperBound = 1000.00, tickUnit = 100.00)
   
   val chart = LineChart[Number, Number](xAxis, yAxis)
   chart.padding = Insets(6)
@@ -31,7 +29,7 @@ class RepairChartDialog(conf: Config, model: Model) extends Dialog[Unit]:
   val avgCost = (repairs.map(r => r.cost).sum / repairs.length).toInt
 
   val series = new XYChart.Series[Number, Number]:
-    name = s"${conf.getString("min")} $minCost  ${conf.getString("max")} $maxCost  ${conf.getString("avg")} $avgCost"
+    name = s"${context.min} $minCost  ${context.max} $maxCost  ${context.avg} $avgCost"
 
   repairs foreach { repair => 
     series.data() += XYChart.Data[Number, Number](repair.on.format(dateFormatter).toDouble, repair.cost) 
@@ -44,5 +42,5 @@ class RepairChartDialog(conf: Config, model: Model) extends Dialog[Unit]:
     spacing = 6; children = List(chart)
 
   initOwner(App.stage)
-  title = conf.getString("repair-chart")
-  headerText = conf.getString("repair-costs")
+  title = context.repairChart
+  headerText = context.repairCosts
