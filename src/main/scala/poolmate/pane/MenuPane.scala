04363a9c828054d5084f55a28854e86c6e327385
objektwerks <objektwerks@runbox.com>
2024-05-17 12:16:29 -0400

    enhanced menu pane

--- src/main/scala/poolmate/pane/MenuPane.scala
+++ src/main/scala/poolmate/pane/MenuPane.scala
@@ -1,29 +1,27 @@
 package poolmate.pane
 
-import com.typesafe.config.Config
-
 import scalafx.application.Platform
 import scalafx.scene.control.Alert.AlertType
 import scalafx.scene.control.{Alert, Menu, MenuBar, MenuItem, SeparatorMenuItem}
 
-import poolmate.App
+import poolmate.{App, Context}
 
-class MenuPane(conf: Config) extends MenuBar:
+class MenuPane(context: Context) extends MenuBar:
   val aboutDialog = new Alert(AlertType.Information):
     initOwner(App.stage)
-    title = conf.getString("about")
-    headerText = conf.getString("developer")
-    contentText = s"${conf.getString("app")} ${conf.getString("license")}"
+    title = context.about
+    headerText = context.developer
+    contentText = s"${context.app} ${context.license}"
 
-  val aboutMenuItem = new MenuItem(conf.getString("about")):
+  val aboutMenuItem = new MenuItem(context.about):
     onAction = { _ => aboutDialog.showAndWait() }
 
   val separator = SeparatorMenuItem()
 
-  val exitMenuItem = new MenuItem(conf.getString("exit")):
+  val exitMenuItem = new MenuItem(context.exit):
     onAction = { _ => Platform.exit() }
   
-  val menu = new Menu(conf.getString("menu")):
+  val menu = new Menu(context.menu):
     items = List(aboutMenuItem, separator, exitMenuItem)
 
   menus = List(menu)
