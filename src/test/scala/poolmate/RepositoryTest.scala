bbc3d3af2765f402d671cd22de2f4d9373c7441c
objektwerks <objektwerks@runbox.com>
2025-07-10 10:10:55 -0400

    enhanced repository test

--- src/test/scala/poolmate/RepositoryTest.scala
+++ src/test/scala/poolmate/RepositoryTest.scala
@@ -5,12 +5,12 @@ import com.typesafe.config.ConfigFactory
 import org.scalatest.funsuite.AnyFunSuite
 
 import slick.basic.DatabaseConfig
-import slick.jdbc.{H2Profile, JdbcProfile}
+import slick.jdbc.JdbcProfile
 
 final class RepositoryTest extends AnyFunSuite:
   test("repository"):
     val config = DatabaseConfig.forConfig[JdbcProfile]("test", ConfigFactory.load("test.conf"))
-    val repository = Repository(config, H2Profile)
+    val repository = Repository(config)
     repository.createSchema()
     repository.dropSchema()
     repository.close()
